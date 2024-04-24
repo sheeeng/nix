9f77f9755609b4569af277bd658a4639bc9744ce
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2024-04-24 11:57:58 +0200

    docs: learn
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home_nix.nix
+++ home_nix.nix
@@ -1,9 +1,6 @@
-{ config, pkgs, ... }:
+# ~/.config/home-manager/home. Nix
 
-# with import <nixpkgs> {};
-# with builtins;
-# with lib;
-# with import <home-manager/modules/lib/dag.nix> { inherit lib; };
+{ config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -20,23 +17,66 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
   home.packages = [
-    mdcat
-    pkgs.bc
-    pkgs.cowsay
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+    pkgs.htop
     pkgs.fortune
-    pkgs.gnupg
-    pkgs.htop
-    pkgs.htop
-    pkgs.jq
     pkgs.lolcat
-    pkgs.ncdu
-    pkgs.termite pkgs.tmux
-    pkgs.unzip
-    pkgs.wget
-    pkgs.whois
-    xclip
+    pkgs.cowsay
   ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ubuntu/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    EDITOR = "nvim";
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -62,10 +102,6 @@
     enable = true;
     userName = "Leonard Sheng Sheng Lee";
     userEmail = "305414+sheeeng@users.noreply.github.com";
-    signing = {
-      key = "305414+sheeeng@users.noreply.github.com";
-      signByDefault = false;
-    };
     extraConfig = {
       pull = {
         rebase = true;
@@ -99,26 +135,6 @@
     };
   };
 
-  programs.ssh = {
-    enable = true;
-    agent = {
-      enable = true;
-      enableGpgSupport = true;
-    };
-    extraConfig = ''
-    Host *
-      AddKeysToAgent yes
-      UseKeychain yes
-      IdentityFile ~/.ssh/id_ed25519
-
-    AllowAgentForwarding yes
-    AllowTcpForwarding yes
-    X11Forwarding yes
-    X11DisplayOffset 10
-    X11UseLocalhost no
-    '';
-  };
-
   programs.starship = {
     enable = true;
     settings.add_newline = false;
@@ -138,7 +154,7 @@
     autosuggestion = {
       enable = true;
     };
-    initExtra = builtins.readFile zsh/initExtra;
+    initExtra = builtins.readFile ./initExtraZsh;
     oh-my-zsh = {
       enable = true;
     };
