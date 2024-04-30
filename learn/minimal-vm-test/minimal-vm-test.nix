ac6899a33e460ce869bdf5cc5e6c015decebae28
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2024-04-30 23:28:29 +0200

    feat(nix): minimal vm test
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

---
+++ learn/minimal-vm-test/minimal-vm-test.nix
@@ -0,0 +1,28 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.testers.runNixOSTest {
+  name = "minimal-vm-test";
+
+  nodes.machine = { config, pkgs, ... }: {
+
+    users.users.alice = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [
+        firefox
+        tree
+      ];
+    };
+
+    system.stateVersion = "23.11";
+  };
+
+  testScript = ''
+    machine.wait_for_unit("default.target")
+    machine.succeed("su -- alice -c 'which firefox'")
+    machine.fail("su -- root -c 'which firefox'")
+  '';
+}
