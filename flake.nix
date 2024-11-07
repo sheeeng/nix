96dc0d1b36b68d02606ecb33a77ce49ff8ef6d4e
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2024-11-07 21:20:06 +0100

    chore(nix): update configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
-  description = "Example Darwin system flake";
+  description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:lnl7/nix-darwin";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
