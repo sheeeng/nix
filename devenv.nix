b44337038a9e43102eff3d979253d8870f78f245
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2024-11-07 19:14:36 +0100

    feat(nix): devenv configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,25 +1,37 @@
-{ pkgs, lib, config, inputs, ... }:
-
+{ pkgs, ... }:
 {
-  devenv.debug = true; # https://devenv.sh/reference/options/#devenvdebug
+  devenv.debug = false; # https://devenv.sh/reference/options/#devenvdebug
   devenv.flakesIntegration = true; # https://devenv.sh/reference/options/#devenvflakesintegration
 
   languages.nix.enable = true; # https://devenv.sh/reference/options/#languagesnixenable
   languages.nix.lsp.package = pkgs.nil; # https://devenv.sh/reference/options/#languagesnixlsppackage
 
-  pre-commit.hooks = { # https://devenv.sh/reference/options/#pre-commithooks
-    my-nixfmt-rfc-style = {
-      enable = true;
-      name = "my-nixfmt-rfc-style";
-      description = "nixfmt-rfc-style";
-      files = "\.nix$";
-      entry = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
-    };
-
+  pre-commit.hooks = {
+    # https://devenv.sh/reference/options/#pre-commithooks
     # Markdown
-    markdownlint.enable = true;
+    markdownlint = {
+      enable = true;
+      settings.configuration = {
+        MD013.line_length = 512;
+      };
+    };
     mdl.enable = true;
+    mdl.entry = "${pkgs.mdl}/bin/mdl --style .mdlrc.rb";
     mdsh.enable = true;
+
+    # Nix
+    alejandra = {
+      enable = false;
+      fail_fast = true;
+      excludes = [ "vendor/" ];
+    };
+    deadnix.enable = true;
+    flake-checker.enable = true;
+    nil.enable = true;
+    nixfmt-classic.enable = false;
+    nixfmt-rfc-style.enable = true;
+    nixpkgs-fmt.enable = true;
+    statix.enable = true;
 
     # YAML
     check-yaml.enable = true;
@@ -36,47 +48,36 @@
     pretty-format-json.enable = true;
   };
 }
-
 # {
 #   # https://devenv.sh/basics/
 #   env.GREET = "devenv";
-
 #   # https://devenv.sh/packages/
 #   packages = [ pkgs.git ];
-
 #   # https://devenv.sh/languages/
 #   # languages.rust.enable = true;
-
 #   # https://devenv.sh/processes/
 #   # processes.cargo-watch.exec = "cargo-watch";
-
 #   # https://devenv.sh/services/
 #   # services.postgres.enable = true;
-
 #   # https://devenv.sh/scripts/
 #   scripts.hello.exec = ''
 #     echo hello from $GREET
 #   '';
-
 #   enterShell = ''
 #     hello
 #     git --version
 #   '';
-
 #   # https://devenv.sh/tasks/
 #   # tasks = {
 #   #   "myproj:setup".exec = "mytool build";
 #   #   "devenv:enterShell".after = [ "myproj:setup" ];
 #   # };
-
 #   # https://devenv.sh/tests/
 #   enterTest = ''
 #     echo "Running tests"
 #     git --version | grep --color=auto "${pkgs.git.version}"
 #   '';
-
 #   # https://devenv.sh/pre-commit-hooks/
 #   # pre-commit.hooks.shellcheck.enable = true;
-
 #   # See full reference at https://devenv.sh/reference/options/
 # }
