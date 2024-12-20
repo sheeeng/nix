8aca40b3b2dbacd32d61cbb54f3993b68703f9a7
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2024-12-20 11:03:40 +0100

    feat(nix): nixvim configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

---
+++ home-manager/packages/nixvim/keymaps.nix
@@ -0,0 +1,125 @@
+# https://github.com/mufaroxyz/dotfiles/blob/249b67c429aa4d25016887d6c337ec5b6d355b46/modules/core/nixvim/keymaps.nix
+
+{ ... }:
+{
+  programs.nixvim.keymaps = [
+    # Neo-tree bindings
+    {
+      action = "<cmd>Neotree toggle<CR>";
+      key = "<leader>e";
+    }
+
+    # Undotree
+    {
+      mode = "n";
+      key = "<leader>ut";
+      action = "<cmd>UndotreeToggle<CR>";
+      options = {
+        desc = "Undotree";
+      };
+    }
+
+    # Lazygit
+    {
+      mode = "n";
+      key = "<leader>gg";
+      action = "<cmd>LazyGit<CR>";
+      options = {
+        desc = "LazyGit (root dir)";
+      };
+    }
+
+    # Commentary bindings
+    {
+      action = "<cmd>Commentary<CR>";
+      key = "<leader>/";
+    }
+
+    # Telescope bindings
+
+    {
+      action = "<cmd>Telescope live_grep<CR>";
+      key = "<leader>fw";
+    }
+    {
+      action = "<cmd>Telescope find_files<CR>";
+      key = "<leader>ff";
+    }
+    {
+      action = "<cmd>Telescope git_commits<CR>";
+      key = "<leader>fg";
+    }
+    {
+      action = "<cmd>Telescope oldfiles<CR>";
+      key = "<leader>fh";
+    }
+    {
+      action = "<cmd>Telescope colorscheme<CR>";
+      key = "<leader>ch";
+    }
+    {
+      action = "<cmd>Telescope man_pages<CR>";
+      key = "<leader>fm";
+    }
+
+    # Notify bindings
+
+    {
+      mode = "n";
+      key = "<leader>un";
+      action = ''
+        <cmd>lua require("notify").dismiss({ silent = true, pending = true })<cr>
+      '';
+      options = {
+        desc = "Dismiss All Notifications";
+      };
+    }
+
+    # Bufferline bindings
+
+    {
+      mode = "n";
+      key = "<Tab>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-Tab>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-l>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-h>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bd";
+      action = "<cmd>bdelete<cr>";
+      options = {
+        desc = "Delete buffer";
+      };
+    }
+  ]; # https://nix-community.github.io/nixvim/keymaps/index.html#keymaps
+}
