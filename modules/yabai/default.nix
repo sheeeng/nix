76345f22ef981cf85a90978195e1693d9d402086
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2024-12-23 14:00:09 +0100

    feat(nix): nix-darwin configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

---
+++ modules/yabai/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [ skhd ];
+
+  services.yabai = {
+    enable = true;
+    extraConfig = builtins.readFile ./yabairc;
+  };
+
+  services.skhd = {
+    enable = true;
+    skhdConfig = builtins.readFile ./skhdrc;
+  };
+}
