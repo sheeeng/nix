5aa031a58ae0c4b5e448bcc606d37a26395bec24
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2024-12-24 14:07:15 +0100

    feat(nix): nix-darwin configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

---
+++ modules/nixos/default.nix
@@ -0,0 +1,102 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    inherit (config.nixpkgs) system;
+    config.allowUnfree = true;
+  };
+in
+{
+  imports = [
+    # (modulesPath + "/installer/scan/not-detected.nix")
+    inputs.home-manager.nixosModules.default
+  ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # Limit the number of generations to keep
+  boot.loader.systemd-boot.configurationLimit = 10;
+
+  # Install the latest linux kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/vg/root";
+      fsType = "ext4";
+    };
+    "/home" = {
+      device = "/dev/vg/home";
+      fsType = "ext4";
+    };
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "24.11";
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nix.optimise.automatic = true;
+  nix.settings.auto-optimise-store = true;
+
+  services.lvm.enable = true;
+
+  networking = {
+    useNetworkd = true;
+    wireless.iwd.enable = true;
+    interfaces.wlan0.useDHCP = true;
+    firewall.enable = true;
+  };
+
+  time.timeZone = "Europe/Oslo";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "nb_NO.UTF-8";
+    };
+  };
+
+  security.polkit.enable = true;
+
+  # https://nixos.wiki/wiki/Bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.extraSpecialArgs = {
+    inherit pkgs-unstable;
+  };
+
+  home-manager.users.leonard = ./home-manager.nix;
+
+  users.users.leonard = {
+    group = "wheel";
+    isSystemUser = true;
+  };
+}
