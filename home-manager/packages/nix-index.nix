5167742b893d1df52919e73095fc53f166ee4098
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2024-12-28 21:08:29 +0100

    feat(nix): home-manager configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/nix-index.nix
+++ home-manager/packages/nix-index.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.nix-index.enable = true;
+  programs.nix-index = {
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nix-index.enable
+    enableBashIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nix-index.enableBashIntegration
+    enableFishIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nix-index.enableFishIntegration
+    enableZshIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nix-index.enableZshIntegration
+    package = pkgs.nix-index; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nix-index.package
+  };
 }
