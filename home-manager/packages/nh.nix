c55eda0a69189bd6074280f2298e2059bd108375
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2024-12-28 21:08:29 +0100

    feat(nix): home-manager configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/nh.nix
+++ home-manager/packages/nh.nix
@@ -1,12 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.nh = {
-    enable = true;
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.enable
+    package = pkgs.nh; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.package
     clean = {
-      enable = true;
-      extraArgs = "--keep-since 4d --keep 3";
-      dates = "Mon..Fri *-*-* 07:00:00";
+      enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.enable
+      dates = "Mon..Fri *-*-* 07:00:00"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.dates
+      extraArgs = "--keep-since 4d --keep 3"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.extraArgs
     };
-    flake = "${config.home.homeDirectory}/github/nix";
+    flake = "${config.home.homeDirectory}/github/nix"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.flake
   };
 }
