293c38fb700c9bac37b059c6caa1fe7ebe350cb7
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-01-02 13:15:00 +0100

    feat(nix): home-manager configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -5,170 +5,94 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    age
-    age-plugin-yubikey
-    asdf-vm
-    autoconf
-    azure-cli
-    barcode
-    bc
-    binutils
-    cbonsai
-    chafa
-    cmatrix
-    conform
-    cowsay
-    devenv
-    dos2unix
-    exiftool
-    ffmpeg
-    findutils
-    ipcalc
-    graphviz
-    gzip
-    # htop
-    imagemagick
-    jq
-    krabby
-    lolcat
-    moreutils
-    # mpc-cli
-    # mpd
-    # mkdocs
-    # ncdu # TODO: https://github.com/NixOS/nixpkgs/issues/290512
-    # ncmpcpp
-    # https://nixos.wiki/wiki/fonts#Installing_only_specific_nerdfonts
-    (nerdfonts.override {
-      fonts = [
-        "DejaVuSansMono"
-        "DroidSansMono"
-        "FantasqueSansMono"
-        "FiraCode"
-        "FiraMono"
-        "Hack"
-        "Iosevka"
-        "IosevkaTerm"
-        "IosevkaTermSlab"
-        "NerdFontsSymbolsOnly"
-        "Ubuntu"
-        "UbuntuMono"
-        "UbuntuSans"
-        "VictorMono"
-      ];
-    })
-    # # https://github.com/NixOS/nixpkgs/blob/9018c7b154ab3427970dbfe52d8a3150e0cecb7b/nixos/doc/manual/release-notes/rl-2505.section.md#L53-L57
-    # # nerd-fonts.0xproto # TODO: Avoid nix-fmt formatting.
-    # # nerd-fonts._3270
-    # # nerd-fonts.agave
-    # # nerd-fonts.anonymice
-    # # nerd-fonts.arimo
-    # # nerd-fonts.aurulent-sans-mono
-    # # nerd-fonts.bigblue-terminal
-    # # nerd-fonts.bitstream-vera-sans-mono
-    # # nerd-fonts.blex-mono
-    # # nerd-fonts.caskaydia-cove
-    # # nerd-fonts.caskaydia-mono
-    # # nerd-fonts.code-new-roman
-    # # nerd-fonts.comic-shanns-mono
-    # # nerd-fonts.commit-mono
-    # # nerd-fonts.cousine
-    # # nerd-fonts.d2coding
-    # # nerd-fonts.daddy-time-mono
-    # # nerd-fonts.departure-mono
-    # nerd-fonts.dejavu-sans-mono
-    # nerd-fonts.droid-sans-mono
-    # # nerd-fonts.envy-code-r
-    # nerd-fonts.fantasque-sans-mono
-    # nerd-fonts.fira-code
-    # nerd-fonts.fira-mono
-    # # nerd-fonts.geist-mono
-    # # nerd-fonts.go-mono
-    # # nerd-fonts.gohufont
-    # nerd-fonts.hack
-    # # nerd-fonts.hasklug
-    # # nerd-fonts.heavy-data
-    # # nerd-fonts.hurmit
-    # # nerd-fonts.im-writing
-    # # nerd-fonts.inconsolata
-    # # nerd-fonts.inconsolata-go
-    # # nerd-fonts.inconsolata-lgc
-    # # nerd-fonts.intone-mono
-    # nerd-fonts.iosevka
-    # nerd-fonts.iosevka-term
-    # nerd-fonts.iosevka-term-slab
-    # nerd-fonts.jetbrains-mono
-    # # nerd-fonts.lekton
-    # # nerd-fonts.liberation
-    # # nerd-fonts.lilex
-    # # nerd-fonts.martian-mono
-    # # nerd-fonts.meslo-lg
-    # # nerd-fonts.monaspace
-    # # nerd-fonts.monofur
-    # # nerd-fonts.monoid
-    # # nerd-fonts.mononoki
-    # # nerd-fonts.mplus
-    # nerd-fonts.noto
-    # # nerd-fonts.open-dyslexic
-    # # nerd-fonts.overpass
-    # # nerd-fonts.profont
-    # # nerd-fonts.proggy-clean-tt
-    # # nerd-fonts.recursive-mono
-    # # nerd-fonts.roboto-mono
-    # # nerd-fonts.shure-tech-mono
-    # # nerd-fonts.sauce-code-pro
-    # # nerd-fonts.space-mono
-    # nerd-fonts.symbols-only
-    # # nerd-fonts.terminess-ttf
-    # # nerd-fonts.tinos
-    # nerd-fonts.ubuntu
-    # nerd-fonts.ubuntu-mono
-    # nerd-fonts.ubuntu-sans
-    # nerd-fonts.victor-mono
-    # # nerd-fonts.zed-mono
-    newsboat
-    nil
-    nixd
-    nixfmt-rfc-style
-    nixpkgs-fmt
-    nix-output-monitor
-    obsidian
-    (lib.hiPrio parallel) # https://haseebmajid.dev/posts/2023-10-02-til-how-to-fix-package-binary-collisions-on-nix/
-    pass
-    pass-git-helper
-    passage
-    passphrase2pgp
-    pipes
-    pipes-rs
-    podman
-    podman-compose
-    progress
-    #
-    # (python3.withPackages (
-    #   pkgs: with pkgs; [
-    #     black # python formatting
-    #     flake8 # python linting
-    #   ]
-    # )) # https://github.com/timokau/dotfiles/blob/c2c55834a3b479132ca07794f75a1d887fa29df6/home/configuration.nix#L94-L105
-    #
-    # (python3.withPackages (pythonPackages: [
-    #   pythonPackages.black
-    #   pythonPackages.flake8
-    # ])) # https://discourse.nixos.org/t/how-install-python-packages-globally-with-home-manager/37025/2
-    #
-    rclone # nixpkgs.overlays
-    sipcalc
-    sops
-    stow
-    tenki
-    terraform
-    tldr
-    tree
-    treefmt
-    unzip
-    watch
-    yubikey-manager
-    yubikey-personalization
-    zip
-    wget
+    age # https://search.nixos.org/packages?channel=unstable&type=packages&show=age
+    age-plugin-yubikey # https://search.nixos.org/packages?channel=unstable&type=packages&show=age-plugin-yubikey
+    asdf-vm # https://search.nixos.org/packages?channel=unstable&type=packages&show=asdf-vm
+    autoconf # https://search.nixos.org/packages?channel=unstable&type=packages&show=autoconf
+    azure-cli # https://search.nixos.org/packages?channel=unstable&type=packages&show=azure-cli
+    barcode # https://search.nixos.org/packages?channel=unstable&type=packages&show=barcode
+    bc # https://search.nixos.org/packages?channel=unstable&type=packages&show=bc
+    binutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=binutils
+    btop # https://search.nixos.org/packages?channel=unstable&type=packages&show=btop
+    cbonsai # https://search.nixos.org/packages?channel=unstable&type=packages&show=cbonsai
+    chafa # https://search.nixos.org/packages?channel=unstable&type=packages&show=chafa
+    cmatrix # https://search.nixos.org/packages?channel=unstable&type=packages&show=cmatrix
+    conform # https://search.nixos.org/packages?channel=unstable&type=packages&show=conform
+    cowsay # https://search.nixos.org/packages?channel=unstable&type=packages&show=cowsay
+    croc # https://search.nixos.org/packages?channel=unstable&type=packages&show=croc
+    dasel # https://search.nixos.org/packages?channel=unstable&type=packages&show=dasel
+    devenv # https://search.nixos.org/packages?channel=unstable&type=packages&show=devenv
+    dos2unix # https://search.nixos.org/packages?channel=unstable&type=packages&show=dos2unix
+    exiftool # https://search.nixos.org/packages?channel=unstable&type=packages&show=exiftool
+    ffmpeg # https://search.nixos.org/packages?channel=unstable&type=packages&show=ffmpeg
+    findutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=findutils
+    fq # https://search.nixos.org/packages?channel=unstable&type=packages&show=fq
+    # fuff # TODO: https://github.com/ffuf/ffuf # https://search.nixos.org/packages?channel=unstable&type=packages&show=fuff
+    go-task # https://search.nixos.org/packages?channel=unstable&type=packages&show=go-task
+    gomtree # https://search.nixos.org/packages?channel=unstable&type=packages&show=gomtree
+    gotop # https://search.nixos.org/packages?channel=unstable&type=packages&show=gotop
+    graphviz # https://search.nixos.org/packages?channel=unstable&type=packages&show=graphviz
+    gzip # https://search.nixos.org/packages?channel=unstable&type=packages&show=gzip
+    htop # https://search.nixos.org/packages?channel=unstable&type=packages&show=htop
+    imagemagick # https://search.nixos.org/packages?channel=unstable&type=packages&show=imagemagick
+    ipcalc # https://search.nixos.org/packages?channel=unstable&type=packages&show=ipcalc
+    jq # https://search.nixos.org/packages?channel=unstable&type=packages&show=jq
+    krabby # https://search.nixos.org/packages?channel=unstable&type=packages&show=krabby
+    lazygit # https://search.nixos.org/packages?channel=unstable&type=packages&show=lazygit
+    lazysql # https://search.nixos.org/packages?channel=unstable&type=packages&show=lazysql
+    lego # https://search.nixos.org/packages?channel=unstable&type=packages&show=lego
+    lolcat # https://search.nixos.org/packages?channel=unstable&type=packages&show=lolcat
+    mc # https://search.nixos.org/packages?channel=unstable&type=packages&show=mc
+    minify # https://search.nixos.org/packages?channel=unstable&type=packages&show=minify
+    mkdocs # https://search.nixos.org/packages?channel=unstable&type=packages&show=mkdocs
+    moreutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=moreutils
+    # mpc-cli # TODO: https://github.com/NixOS/nixpkgs/issues/355495 # https://search.nixos.org/packages?channel=unstable&type=packages&show=mpc-cli
+    mpd # https://search.nixos.org/packages?channel=unstable&type=packages&show=mpd
+    # ncdu # TODO: https://github.com/NixOS/nixpkgs/issues/290512 # https://search.nixos.org/packages?channel=unstable&type=packages&show=ncdu
+    ncmpcpp  # https://search.nixos.org/packages?channel=unstable&type=packages&show=ncmpcpp
+    newsboat # https://search.nixos.org/packages?channel=unstable&type=packages&show=newsboat
+    nh # https://search.nixos.org/packages?channel=unstable&type=packages&show=nh
+    nil # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
+    nix # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix
+    nix-output-monitor # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-output-monitor
+    nixd # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixd
+    nixfmt-rfc-style # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixfmt-rfc-style
+    nixpkgs-fmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixpkgs-fmt
+    obsidian # https://search.nixos.org/packages?channel=unstable&type=packages&show=obsidian
+    # parallel # (lib.hiPrio parallel) # TODO: https://haseebmajid.dev/posts/2023-10-02-til-how-to-fix-package-binary-collisions-on-nix/ # https://search.nixos.org/packages?channel=unstable&type=packages&show=parallel
+    pass # https://search.nixos.org/packages?channel=unstable&type=packages&show=pass
+    pass-git-helper # https://search.nixos.org/packages?channel=unstable&type=packages&show=pass-git-helper
+    passage # https://search.nixos.org/packages?channel=unstable&type=packages&show=passage
+    passphrase2pgp # https://search.nixos.org/packages?channel=unstable&type=packages&show=passphrase2pgp
+    pipes # https://search.nixos.org/packages?channel=unstable&type=packages&show=pipes
+    pipes-rs # https://search.nixos.org/packages?channel=unstable&type=packages&show=pipes-rs
+    podman # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman
+    podman-compose # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman-compose
+    progress # https://search.nixos.org/packages?channel=unstable&type=packages&show=progress
+    qrencode # https://search.nixos.org/packages?channel=unstable&type=packages&show=qrencode
+    rclone # TODO: See `nixpkgs.overlays` in other file. # https://search.nixos.org/packages?channel=unstable&type=packages&show=rclone
+    sipcalc # https://search.nixos.org/packages?channel=unstable&type=packages&show=sipcalc
+    sops # https://search.nixos.org/packages?channel=unstable&type=packages&show=sops
+    spruce # https://search.nixos.org/packages?channel=unstable&type=packages&show=spruce
+    sqlite # https://search.nixos.org/packages?channel=unstable&type=packages&show=sqlite
+    ssss # https://search.nixos.org/packages?channel=unstable&type=packages&show=ssss
+    stow # https://search.nixos.org/packages?channel=unstable&type=packages&show=stow
+    # taskctl # TODO: https://github.com/taskctl/taskctl # https://search.nixos.org/packages?channel=unstable&type=packages&show=taskctl
+    tenki # https://search.nixos.org/packages?channel=unstable&type=packages&show=tenki
+    terraform # https://search.nixos.org/packages?channel=unstable&type=packages&show=terraform
+    tldr # https://search.nixos.org/packages?channel=unstable&type=packages&show=tldr
+    tree # https://search.nixos.org/packages?channel=unstable&type=packages&show=tree
+    treefmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=treefmt
+    unzip # https://search.nixos.org/packages?channel=unstable&type=packages&show=unzip
+    # uuid7 # TODO: https://github.com/stevesimmons/uuid7 # https://search.nixos.org/packages?channel=unstable&type=packages&show=uuid7
+    vhs # https://search.nixos.org/packages?channel=unstable&type=packages&show=vhs
+    watch # https://search.nixos.org/packages?channel=unstable&type=packages&show=watch
+    websocat # https://search.nixos.org/packages?channel=unstable&type=packages&show=websocat
+    wget # https://search.nixos.org/packages?channel=unstable&type=packages&show=wget
+    # ws # TODO: https://github.com/lewoudar/ws/ # https://search.nixos.org/packages?channel=unstable&type=packages&show=ws
+    yubikey-manager # https://search.nixos.org/packages?channel=unstable&type=packages&show=yubikey-manager
+    yubikey-personalization # https://search.nixos.org/packages?channel=unstable&type=packages&show=yubikey-personalization
+    zenity # https://search.nixos.org/packages?channel=unstable&type=packages&show=zenity
+    zip # https://search.nixos.org/packages?channel=unstable&type=packages&show=zip
   ];
 }
