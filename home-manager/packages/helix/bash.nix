aa53fbf5179c61cc9f5552f02623142a2a22561f
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-01-08 20:46:57 +0100

    feat(nix): helix editor configuration
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/bash.nix
+++ home-manager/packages/helix/bash.nix
@@ -1,16 +1,30 @@
 # https://github.com/tommy-donavon/nixos-dots/blob/d824d5ec55109f65f0bc5e042198cafde0fbedc8/modules/home/programs/terminal/editors/helix/nix.nix
 
 { pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    bash-language-server
-  ];
-
   programs.helix.languages = {
-    language-server.bash-language-server = {
-      args = [ "start" ];
-      command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+    language = [
+      {
+        name = "bash";
+        auto-format = true;
+        formatter = {
+          command = "${pkgs.shfmt}/bin/shfmt";
+          args = [
+            "--binary-next-line"
+            "--case-indent"
+            "--indent 2"
+            "--simplify"
+            "--space-redirects"
+          ];
+        };
+      }
+    ];
+
+    language-server = {
+      bash-language-server = {
+        args = [ "start" ];
+        command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+      };
     };
   };
 }
