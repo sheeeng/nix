20c092950143638dc60d144974b63c293d1cb6ac
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-01-16 17:12:30 +0100

    refactor: directories and files
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/yaml.nix
+++ home-manager/packages/helix/yaml.nix
@@ -20,9 +20,11 @@
           };
           config.yaml.schemas = {
             # ansible tasks
-            "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks" = "tasks/*.{yml,yaml}";
+            "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks" =
+              "tasks/*.{yml,yaml}";
             # ansible playbooks
-            "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook" = "*ansible*/*.{yml,yaml}";
+            "https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook" =
+              "*ansible*/*.{yml,yaml}";
             # kustomization
             "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
             # helm chart
@@ -30,9 +32,11 @@
             # helmfile
             "http://json.schemastore.org/helmfile.json" = "helmfile.{yml,yaml}";
             # gitlab-ci
-            "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" = "*gitlab-ci*.{yml,yaml}";
+            "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" =
+              "*gitlab-ci*.{yml,yaml}";
             # argo workflows
-            "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+            "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
+              "*flow*.{yml,yaml}";
             # taskfile
             "http://json.schemastore.org/taskfile.json" = "Taskfile*.{yml,yaml}";
             # kubernetes
@@ -42,11 +46,14 @@
             # github actions
             "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
             # flux kustomization
-            "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json" = "ks.{yml,yaml}";
+            "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json" =
+              "ks.{yml,yaml}";
             # flux helmrelease
-            "https://kubernetes-schemas.pages.dev/helm.toolkit.fluxcd.io/helmrelease_v2beta2.json" = "helmrelease.{yml,yaml}";
+            "https://kubernetes-schemas.pages.dev/helm.toolkit.fluxcd.io/helmrelease_v2beta2.json" =
+              "helmrelease.{yml,yaml}";
             # externalsecrets
-            "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/external-secrets.io/externalsecret_v1beta1.json" = "external*secret.{yml,yaml}";
+            "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/external-secrets.io/externalsecret_v1beta1.json" =
+              "external*secret.{yml,yaml}";
           };
         };
       }; # https://github.com/brenix/nix-config/blob/85dd440897b731a338b7a85a0bd038b83091682e/modules/home/programs/terminal/editors/helix/languages.nix#L15-L52
