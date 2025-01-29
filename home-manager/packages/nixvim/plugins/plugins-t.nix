15c65d01b294babb488771945ed005b7a37d428b
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-01-29 14:48:03 +0100

    chore(nix): organize nixvim plugins
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/nixvim/plugins/plugins-t.nix
+++ home-manager/packages/nixvim/plugins/plugins-t.nix
@@ -1,4 +1,26 @@
 { ... }:
 {
-  programs.nixvim.plugins = { };
+  programs.nixvim.plugins = {
+    telescope = {
+      enable = true; # https://nix-community.github.io/nixvim/plugins/telescope/index.html#pluginstelescopeenable
+      extensions = {
+        fzf-native = {
+          enable = true;
+        };
+      };
+    }; # https://nix-community.github.io/nixvim/plugins/telescope/index.html
+
+    toggleterm = {
+      enable = true; # https://nix-community.github.io/nixvim/plugins/toggleterm/index.html#pluginstoggletermenable
+      settings = {
+        hide_numbers = false;
+        autochdir = true;
+        close_on_exit = true;
+        direction = "vertical";
+      };
+    };
+
+    treesitter.enable = true;
+    ts-autotag.enable = true;
+  };
 }
