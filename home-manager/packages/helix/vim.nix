1db9552c24a8dea02a609e63862ab89c905742dc
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-01-29 12:35:15 +0100

    fix(nix): add scope field
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/vim.nix
+++ home-manager/packages/helix/vim.nix
@@ -5,10 +5,13 @@
   programs.helix = {
     languages = {
       language = [ ];
-      language-server = {
-        vim-language-server = {
-          command = lib.getExe pkgs.vim-language-server; # https://search.nixos.org/packages?channel=unstable&type=packages&show=vim-language-server
-          args = lib.singleton "--stdio";
+      vim = {
+        scope = "source.vim";
+        language-server = {
+          vim-language-server = {
+            command = "${pkgs.vim-language-server}/bin/vim-language-server"; # https://search.nixos.org/packages?channel=unstable&type=packages&show=vim-language-server
+            args = lib.singleton "--stdio";
+          };
         };
       };
     }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.helix.languages
