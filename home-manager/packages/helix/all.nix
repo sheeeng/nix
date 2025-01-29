0b703c5b184a41298c49fcf6348eac522d174edc
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-01-29 20:13:07 +0100

    chore(nix): ignore fallback glob
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/all.nix
+++ home-manager/packages/helix/all.nix
@@ -5,10 +5,9 @@
   programs.helix = {
     # languages = {
     #   language = {
-    #     name = "fallback";
+    #     name = "all";
     #     scope = "";
     #     file-types = [ { glob = "*"; } ];
-    #     language-servers = [ ];
     #   }; # TODO: https://github.com/helix-editor/helix/issues/9165#issuecomment-2310454339
     #   language-server = { };
     # }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.helix.languages
--- home-manager/packages/helix/default.nix
+++ home-manager/packages/helix/default.nix
@@ -9,10 +9,10 @@
   # https://snowfall.org/reference/lib/#libsnowfallfsget-non-default-nix-files
   # imports = lib.snowfall.fs.get-non-default-nix-files ./.; # https://github.com/tommy-donavon/nixos-dots/blob/d824d5ec55109f65f0bc5e042198cafde0fbedc8/modules/home/programs/terminal/editors/helix/default.nix#L15
   imports = [
+    ./all.nix
     ./bash.nix
     ./docker.nix
     ./elixir.nix
-    ./fallback.nix
     ./go.nix
     ./gpt.nix
     ./json.nix
