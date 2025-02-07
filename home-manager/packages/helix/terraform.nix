ed59a5145f9a7c5f868bddff0e09352af8abc462
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-02-07 14:58:53 +0100

    feat(nix): neovim
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/terraform.nix
+++ home-manager/packages/helix/terraform.nix
@@ -1,8 +1,29 @@
-{ lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    terraform # https://search.nixos.org/packages?channel=unstable&type=packages&show=terraform
-    terraform-ls # https://search.nixos.org/packages?channel=unstable&type=packages&show=terraform-ls
+  lib,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
+let
+  buffer-language-server = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "buffer-language-server";
+    version = "main";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "metafates";
+      repo = "buffer-language-server";
+      rev = "main";
+      sha256 = "sha256-5q1dEkKRi8SYTvNyEND0S23xPeqV8b5C2Mm+0jLWIpA=";
+    };
+
+    cargoHash = "sha256-dOJef0yKgzgbt80Ckl/AC/nkT97sBhv/VMxXArftj+k=";
+  };
+in
+{
+  home.packages = with pkgs-unstable; [
+    # terraform # https://search.nixos.org/packages?channel=unstable&type=packages&show=terraform
+    # terraform-ls # https://search.nixos.org/packages?channel=unstable&type=packages&show=terraform-ls
+    buffer-language-server
   ];
 
   # https://github.com/helix-editor/helix/blob/master/languages.toml
@@ -29,11 +50,11 @@
         }
       ];
       language-server = {
-        # buffer-language-server = {
-        #   command = "buffer-language-server";
-        # };
+        buffer-language-server = {
+          command = "${buffer-language-server}/bin/buffer-language-server";
+        };
         terraform-ls = {
-          command = lib.getExe pkgs.terraform-ls; # "${pkgs.terraform-ls}/bin/terraform-ls";
+          command = lib.getExe pkgs-unstable.terraform-ls; # "${pkgs.terraform-ls}/bin/terraform-ls";
           args = [ "serve" ];
           filetypes = [
             "hcl"
---
+++ home-manager/packages/neovim/init.lua
@@ -0,0 +1,53 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Hide the status line completely
+vim.opt.laststatus = 0
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Number width
+vim.opt.numberwidth = 1
+
+-- Mouse
+vim.opt.mouse = ""
+
+-- Auto-indentation
+vim.opt.autoindent = true
+
+-- Tabs and indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.smarttab = true
+
+-- Case sensitivity
+vim.opt.smartcase = true
+
+-- Expand tab to spaces
+vim.opt.expandtab = true
+
+-- Encoding
+vim.opt.encoding = "utf-8"
+
+-- Clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- Disable textwidth and colorcolumn for Git commit messages
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "gitcommit",
+	callback = function()
+		vim.opt_local.textwidth = 0
+		vim.opt_local.colorcolumn = ""
+	end,
+})
+
+-- Make the background transparent
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
