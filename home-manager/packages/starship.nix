e5e9fd6d7a21af951dfb803208e63cbf65982a46
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-02-22 14:22:13 +0100

    chore(nix): configure fish
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/starship.nix
+++ home-manager/packages/starship.nix
@@ -1,22 +1,22 @@
 # https://github.com/RMTT/machines/blob/8a1d5a5c62e1e4e6b5e48184bc11960fce56fb24/home/modules/shell.nix
-_: { }
+# _: { }
 
-# { ... }:
-# {
-#   programs.starship = {
-#     enable = true;
-#     settings = {
-#       add_newline = false;
-#       character.success_symbol = "[>](bold green)";
-#       character.error_symbol = "[>](bold red)";
-#       character.vimcmd_symbol = "[<](bold green)";
-#       character.vimcmd_replace_one_symbol = "[<](bold purple)";
-#       character.vimcmd_replace_symbol = "[<](bold purple)";
-#       character.vimcmd_visual_symbol = "[<](bold yellow)";
+{ ... }:
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character.success_symbol = "[>](bold green)";
+      character.error_symbol = "[>](bold red)";
+      character.vimcmd_symbol = "[<](bold green)";
+      character.vimcmd_replace_one_symbol = "[<](bold purple)";
+      character.vimcmd_replace_symbol = "[<](bold purple)";
+      character.vimcmd_visual_symbol = "[<](bold yellow)";
 
-#       package.disabled = true;
-#       gcloud.disabled = true;
-#       kubernetes.disabled = false;
-#     };
-#   };
-# }
+      package.disabled = false;
+      gcloud.disabled = false;
+      kubernetes.disabled = false;
+    };
+  };
+}
