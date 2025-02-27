5f08e0acca656944a55079bb7f0a806881acb220
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-02-27 11:51:38 +0100

    feat(nix): hclgrep
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

---
+++ home-manager/packages/hclgrep.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+let
+  hclgrep =
+    with pkgs;
+    buildGoModule rec {
+      pname = "hclgrep";
+      version = "00059cbc78022cab225e172cc695c5edf1e4b8ef";
+
+      src = fetchFromGitHub {
+        owner = "magodo";
+        repo = "hclgrep";
+        rev = "00059cbc78022cab225e172cc695c5edf1e4b8ef";
+        hash = "sha256-OzY8QmOpJsfr3jYfzdamUMr9tIZPkuHtq9CfiCnVbAo="; # lib.fakeHash;
+      };
+
+      vendorHash = "sha256-Vk++UnfbKQMzM2uP+VwYVe9zgWhmBWckZ9oYF2Rfk0Q="; # lib.fakeHash;
+    };
+in
+{
+  home.packages = with pkgs; [
+    hclgrep
+  ];
+}
