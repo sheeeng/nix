5e66d9b71b41fef64deebc78325df87e4d8066b7
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-05-15 11:43:01 +0200

    feat(nix): jekyll-theme-minimal
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/ruby.nix
+++ home-manager/packages/ruby.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    bundix
-    jekyll
+    bundix # https://search.nixos.org/packages?channel=unstable&type=packages&show=bundix
+    # bundler # https://search.nixos.org/packages?channel=unstable&type=packages&show=bundler
+    # jekyll # https://search.nixos.org/packages?channel=unstable&type=packages&show=jekyll
     (ruby.withPackages (
       pkgs: with pkgs; [
-        bundler
-        nokogiri
-        rouge
+        github-pages # https://search.nixos.org/packages?channel=unstable&type=packages&show=rubyPackages.github-pages
+        jekyll-theme-minimal # https://search.nixos.org/packages?channel=unstable&type=packages&show=rubyPackages.jekyll-theme-minimal
+        nokogiri # https://search.nixos.org/packages?channel=unstable&type=packages&show=rubyPackages.nokogiri
+        rouge # https://search.nixos.org/packages?channel=unstable&type=packages&show=rubyPackages.rouge
       ]
     ))
   ];
