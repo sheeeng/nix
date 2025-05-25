64f28d7f4d4f14fc286427bf4a7fb2caf607592f
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-05-25 20:18:14 +0100

    feat(nix): upgrade home state version
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- hosts/NHNWCQ17DF/default.nix
+++ hosts/NHNWCQ17DF/default.nix
@@ -179,7 +179,7 @@ in
   ]; # https://nix-community.github.io/home-manager/nixos-options.xhtml#nixos-opt-home-manager.sharedModules
 
   home-manager.users.llee = {
-    home.stateVersion = "25.05";
+    home.stateVersion = "25.11";
     imports = [
       ../../home-manager/home.nix
       inputs.agenix.homeManagerModules.age
