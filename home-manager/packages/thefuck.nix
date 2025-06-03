9a20163399ba36cda418bef394fa3b63c3a8439b
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-06-03 12:25:24 +0200

    feat(nix): pay-respects
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/thefuck.nix
+++ home-manager/packages/thefuck.nix
@@ -4,7 +4,8 @@ in
 {
   programs = {
     thefuck = {
-      enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.thefuck.enable
+      # error: 'thefuck' has been removed due to lack of maintenance upstream and incompatible with python 3.12+. Consider using 'pay-respects' instead
+      enable = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.thefuck.enable
       package = pkgs.thefuck; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.thefuck.package
       enableBashIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.thefuck.enableBashIntegration
       enableFishIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.thefuck.enableFishIntegration
