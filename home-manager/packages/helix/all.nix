65ed25b5ac267c7aad6ccbec7842aacc627637a9
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-06-29 18:18:46 +0200

    chore(nix): flake update
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/helix/all.nix
+++ home-manager/packages/helix/all.nix
@@ -7,50 +7,50 @@
 {
   home.packages = [ ];
 
-  programs.helix = lib.mkIf config.program.helix.enable {
-    extra-packages = with pkgs; [
-      nixpkgs-fmt
-      nixpkgs-rfc-style
-      nixd
-      nil
-    ];
-    ignores = [ ".direnv/" ];
-    languages = {
-      language = [
-        {
-          name = "nix";
-          auto-format = false;
-          file-types = [ "nix" ];
-          roots = [ "flake.lock" ];
-          formatter = {
-            command = "${pkgs.nixpkgs-rfc-style}/bin/nixpkgs-fmt";
-          };
-          indent = {
-            tab-width = 2;
-            unit = "  ";
-          };
-          language-server = "nixd";
-          expect-features = [ "format" ];
-        }
-      ];
-      language-server = {
-        nil = {
-          command = lib.getExe pkgs.nil; # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
-          config = {
-            format = {
-              trimTrailingWhitespace = true;
-            };
-          };
-        };
-        nixd = {
-          command = lib.getExe pkgs.nixd; # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixd
-          config = {
-            format = {
-              trimTrailingWhitespace = true;
-            };
-          };
-        };
-      };
-    };
-  };
+  # programs.helix = lib.mkIf config.programs.helix.enable {
+  #   extraPackages = with pkgs; [
+  #     nixpkgs-fmt
+  #     nixfmt-rfc-style
+  #     nixd
+  #     nil
+  #   ];
+  #   ignores = [ ".direnv/" ];
+  #   languages = {
+  #     language = [
+  #       {
+  #         name = "nix";
+  #         auto-format = false;
+  #         file-types = [ "nix" ];
+  #         roots = [ "flake.lock" ];
+  #         formatter = {
+  #           command = "${pkgs.nixpkgs-rfc-style}/bin/nixpkgs-fmt";
+  #         };
+  #         indent = {
+  #           tab-width = 2;
+  #           unit = "  ";
+  #         };
+  #         language-server = "nixd";
+  #         expect-features = [ "format" ];
+  #       }
+  #     ];
+  #     language-server = {
+  #       nil = {
+  #         command = lib.getExe pkgs.nil; # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
+  #         config = {
+  #           format = {
+  #             trimTrailingWhitespace = true;
+  #           };
+  #         };
+  #       };
+  #       nixd = {
+  #         command = lib.getExe pkgs.nixd; # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixd
+  #         config = {
+  #           format = {
+  #             trimTrailingWhitespace = true;
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }
