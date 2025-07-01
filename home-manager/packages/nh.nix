2bceafd4287072bfd8bd3cf15b5df2f57c165431
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-07-01 09:39:57 +0200

    fix(nix): nh dates
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/packages/nh.nix
+++ home-manager/packages/nh.nix
@@ -6,7 +6,7 @@
     clean = {
       # TODO: programs.nh.clean.enable and nix.gc.automatic (system-wide in configuration.nix) are both enabled. Please use one or the other to avoid conflict.
       enable = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.enable
-      dates = "Mon..Fri *-*-* 07:00:00"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.dates
+      dates = "daily"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.dates
       extraArgs = "--keep-since 4d --keep 3"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.clean.extraArgs
     };
     flake = "${config.home.homeDirectory}/github/nix"; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nh.flake
