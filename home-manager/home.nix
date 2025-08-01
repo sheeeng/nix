3ac28c88408f630b8611ad94f9091c7340f1cbf6
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-08-01 14:04:15 +0200

    chore(nix): nixfmt
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,7 +17,8 @@ in
     ./scripts.nix
     ./secrets/age/age.nix
     ./theme.nix
-  ] ++ (folderFiles ./packages);
+  ]
+  ++ (folderFiles ./packages);
   # https://github.com/alexnabokikh/nix-config/blob/bddec40e097d4227cd95badfc02164aa006a8a4c/modules/home-manager/common/default.nix
 
   fonts.fontconfig.enable = true;
