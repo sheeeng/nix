4218751c7ececa8056754631577a0c52b1914f37
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-08-13 12:34:12 +0200

    chore(nix): dracula theme
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  catppuccin.enable = true;
+  catppuccin.enable = false;
   catppuccin.flavor = "mocha";
   catppuccin.mako.enable = false; # https://discourse.nixos.org/t/use-services-mako-settings-instead/63902
 }
