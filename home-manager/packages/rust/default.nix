c5b868ea71ecdd2b7172360f3595a954dff0d6b7
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-08-25 14:59:30 +0200

    fix(nix): switch failures
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/rust/default.nix
+++ home-manager/packages/rust/default.nix
@@ -7,7 +7,8 @@
 }:
 {
   home.packages = with pkgs; [
-    rustup
+    # cargo # https://search.nixos.org/packages?channel=unstable&type=packages&show=cargo
+    rustup # https://search.nixos.org/packages?channel=unstable&type=packages&show=rustup
   ];
 
   home.sessionVariables.RUSTUP_HOME = "${config.xdg.dataHome}/.rustup"; # https://rust-lang.github.io/rustup/configuration.html
