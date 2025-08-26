b7f8ef1fd5fd31fd85288824ebd6a6dcc53f0a8b
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-08-26 20:03:35 +0200

    chore(nix): fmt
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/helix/terraform.nix
+++ home-manager/packages/helix/terraform.nix
@@ -7,13 +7,13 @@
 let
   buffer-language-server = pkgs.rustPlatform.buildRustPackage rec {
     pname = "buffer-language-server";
-    version = "main";
+    version = "unstable-2023-07-24";
 
     src = pkgs.fetchFromGitHub {
       owner = "metafates";
       repo = "buffer-language-server";
-      rev = "main";
-      sha256 = "sha256-5q1dEkKRi8SYTvNyEND0S23xPeqV8b5C2Mm+0jLWIpA=";
+      rev = "9419175abe59acec129d61c8cbe47a3ebfbce5b0";
+      sha256 = "1412sqrd5gn9v11bxwcmx8yz2vabyk810wpk9scc92wi8895vbg6";
     };
 
     cargoHash = "sha256-CERip5AzZvbF8BkEVq27nJraW0fnF3EgA39ot4RV2pA=";
