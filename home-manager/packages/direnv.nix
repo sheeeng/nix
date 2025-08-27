c6eec8324812667290c56cecd50a09737f710ce7
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-08-27 20:14:29 +0200

    chore(nix): configure direnv
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/direnv.nix
+++ home-manager/packages/direnv.nix
@@ -1,19 +1,17 @@
 { pkgs, ... }:
 {
-  programs = {
-    direnv = {
-      enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enable
-      package = pkgs.direnv; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.package
-      enableBashIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableBashIntegration
-      enableFishIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableFishIntegration
-      enableNushellIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableNushellIntegration
-      enableZshIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableZshIntegration
-      nix-direnv = {
-        enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.nix-direnv.enable
-        package = pkgs.nix-direnv; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.nix-direnv.package
-      };
-      silent = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.silent
-      stdlib = ""; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.stdlib
+  programs.direnv = {
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enable
+    package = pkgs.direnv; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.package
+    enableBashIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableBashIntegration
+    enableFishIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableFishIntegration
+    enableNushellIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableNushellIntegration
+    enableZshIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enableZshIntegration
+    nix-direnv = {
+      enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.nix-direnv.enable
+      package = pkgs.nix-direnv; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.nix-direnv.package
     };
+    silent = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.silent
+    stdlib = ""; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.stdlib
   };
 }
