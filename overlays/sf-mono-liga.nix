d39fb8dae1baa36c823d10304ae34ea14bf774f6
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-04 08:23:08 +0200

    feat(nix): sf mono nerd fonts ligatures
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

---
+++ overlays/sf-mono-liga.nix
@@ -0,0 +1,14 @@
+# SFMono Nerd Font Ligaturized overlay
+# https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized
+inputs: final: prev: {
+  sf-mono-liga-bin = prev.stdenvNoCC.mkDerivation {
+    pname = "sf-mono-liga-bin";
+    version = "dev";
+    src = inputs.sf-mono-liga-src;
+    dontConfigure = true;
+    installPhase = ''
+      mkdir -p $out/share/fonts/opentype
+      cp -R $src/*.otf $out/share/fonts/opentype/
+    '';
+  };
+}
