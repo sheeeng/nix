f446c160a0e41f8721534a2ef6a6dd9bb794249d
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-08 19:08:26 +0200

    chore(nix): install python's identify-cli
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/python.nix
+++ home-manager/packages/python.nix
@@ -9,14 +9,16 @@
       pkgs: with pkgs; [
         black # https://search.nixos.org/packages?channel=unstable&type=packages&show=black
         flake8 # https://search.nixos.org/packages?channel=unstable&type=packages&show=flake8
+        identify # https://search.nixos.org/packages?channel=unstable&type=packages&show=identify
         pip # https://search.nixos.org/packages?channel=unstable&type=packages&show=pip
       ]
     )) # https://github.com/timokau/dotfiles/blob/c2c55834a3b479132ca07794f75a1d887fa29df6/home/configuration.nix#L94-L105
 
     (python3.withPackages (pythonPackages: [
-      pythonPackages.black # https://search.nixos.org/packages?channel=unstable&type=packages&show=black
-      pythonPackages.flake8 # https://search.nixos.org/packages?channel=unstable&type=packages&show=flake8
-      pythonPackages.pip # https://search.nixos.org/packages?channel=unstable&type=packages&show=pip
+      pythonPackages.black # https://search.nixos.org/packages?channel=unstable&type=packages&show=python313Packages.black
+      pythonPackages.flake8 # https://search.nixos.org/packages?channel=unstable&type=packages&show=python313Packages.flake8
+      pythonPackages.identify # https://search.nixos.org/packages?channel=unstable&type=packages&show=python313Packages.identify
+      pythonPackages.pip # https://search.nixos.org/packages?channel=unstable&type=packages&show=python313Packages.pip
     ])) # https://discourse.nixos.org/t/how-install-python-packages-globally-with-home-manager/37025/2
   ];
 }
