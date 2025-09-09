84ff6284ffd31482d7286af6e6aa435f9d6b88fd
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-09 15:23:19 +0200

    chore(nix): disable nodejs tests
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,34 @@
 inputs: {
+  # Skip Node.js tests during nix-darwin switch for improved performance
+  # nodejs-skip-tests = import ./nodejs.nix;
+
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs {
       inherit (final) system;
       config.allowUnfree = true;
     };
+  };
+
+  nodejs-skip-tests = _final: prev: {
+    nodejs = prev.nodejs.overrideAttrs (_: {
+      doCheck = false;
+      doInstallCheck = false;
+      checkPhase = null;
+      installCheckPhase = null;
+    });
+
+    nodejs_20 = prev.nodejs_20.overrideAttrs (_: {
+      doCheck = false;
+      doInstallCheck = false;
+      checkPhase = null;
+      installCheckPhase = null;
+    });
+
+    nodejs_22 = prev.nodejs_22.overrideAttrs (_: {
+      doCheck = false;
+      doInstallCheck = false;
+      checkPhase = null;
+      installCheckPhase = null;
+    });
   };
 }
