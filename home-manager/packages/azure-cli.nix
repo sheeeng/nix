72be8e0d6d2317e67f70d635871d54cbcd3e8cef
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-19 09:58:24 +0200

    chore(nix): configure commands
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/azure-cli.nix
+++ home-manager/packages/azure-cli.nix
@@ -1,6 +1,6 @@
 # https://github.com/maxhbr/myconfig/blob/f8ec80ed9eeb74b9931816f9188c9bd998b3d82f/hosts/host.f13/role.work/azure-cli.nix
 
-# _: { }
+_: { }
 
 # See https://github.com/sheeeng/azure-cli-with-extensions project.
 # {
@@ -20,25 +20,25 @@
 #     })
 #   ];
 
-_: {
-  home.file = {
-    ".azure/config" = {
-      text = ''
-        [core]
-        collect_telemetry = false
-        only_show_errors = false
-        no_color = true
+# _: {
+#   home.file = {
+#     ".azure/config" = {
+#       text = ''
+#         [core]
+#         collect_telemetry = false
+#         only_show_errors = false
+#         no_color = true
 
-        [cloud]
-        name = AzureCloud
+#         [cloud]
+#         name = AzureCloud
 
-        [extension]
-        use_dynamic_install = false
-        run_after_dynamic_install = false
+#         [extension]
+#         use_dynamic_install = false
+#         run_after_dynamic_install = false
 
-        [logging]
-        enable_log_file=no
-      '';
-    };
-  };
-}
+#         [logging]
+#         enable_log_file=no
+#       '';
+#     };
+#   };
+# }
