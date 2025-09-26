905935a1aee1204805fb3733389700dc553f28c3
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-26 08:42:41 +0200

    chore(nix): remove newsboat,neovide
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/newsboat.nix
+++ home-manager/packages/newsboat.nix
@@ -1,8 +1,7 @@
 # https://github.com/legendofmiracles/dotnix/blob/8dfa01af04d6391a1f5cb2c788bdecc1ee748ca9/hm/newsboat.nix
 
-{ pkgs, ... }:
-
-{
+# { pkgs, ... }:
+_: {
   home.file = {
     ".newsboat/urls".text = ''
       http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml
@@ -19,9 +18,9 @@
       # https://api.politiet.no/politiloggen/v1/atom?municipalities=L%C3%B8renskog
       # https://api.politiet.no/politiloggen/v1/rss?municipalities=L%C3%B8renskog
     '';
-    ".newsboat/config".text = ''
-      include ${pkgs.newsboat}/share/doc/newsboat/contrib/colorschemes/plain
-    '';
+    # ".newsboat/config".text = ''
+    #   include ${pkgs.newsboat}/share/doc/newsboat/contrib/colorschemes/plain
+    # '';
   };
 
   # home.packages = [ pkgs.newsboat ];
