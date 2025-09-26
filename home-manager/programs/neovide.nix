609157c0dd110f8e652bc5ad0b9ff79d4e052da7
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-26 08:42:41 +0200

    chore(nix): remove newsboat,neovide
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/neovide.nix
+++ home-manager/programs/neovide.nix
@@ -3,7 +3,7 @@
   # Enable on aarch64-darwin and Linux host platforms.
   # TODO: https://github.com/nix-community/home-manager/issues/6087
   programs.neovide = {
-    enable = pkgs.stdenv.isLinux || (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64); # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.neovide.enable
+    enable = false; # pkgs.stdenv.isLinux || (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64); # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.neovide.enable
     package = pkgs.neovide; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.neovide.package
     settings = {
       fork = false;
