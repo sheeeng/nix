065f5e6af78833a481135fbb50339b905325eff8
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-09-30 21:58:42 +0200

    chore(nix): edit shell
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -9,29 +9,26 @@
   pre-commit = pkgs.mkShell {
     name = "pre-commit-shell";
 
-    # if you set use-xdg-base-directories = true in your /etc/nix/nix.conf,
-    # all the "classic" Nix tools (nix-env, nix-channel, ...)
-    # and nix profile will use ~/.local/state/nix instead of dotfiles in your ~.
-    # https://github.com/NixOS/nix/issues/1079#issuecomment-1426124503
-    # Set use-xdg-base-directories = true in your /etc/nix/nix.conf, and then nix-channel will use ~/.local/state/nix/channels.
-    # https://github.com/NixOS/nix/issues/1079#issuecomment-1632043144
-    NIX_CONFIG = "use-xdg-base-directories = true\nextra-experimental-features = nix-command flakes";
-
-    PKCS = "${pkgs.opensc}/lib/opensc-pkcs11.so";
+    # # if you set use-xdg-base-directories = true in your /etc/nix/nix.conf,
+    # # all the "classic" Nix tools (nix-env, nix-channel, ...)
+    # # and nix profile will use ~/.local/state/nix instead of dotfiles in your ~.
+    # # https://github.com/NixOS/nix/issues/1079#issuecomment-1426124503
+    # # Set use-xdg-base-directories = true in your /etc/nix/nix.conf, and then nix-channel will use ~/.local/state/nix/channels.
+    # # https://github.com/NixOS/nix/issues/1079#issuecomment-1632043144
+    # NIX_CONFIG = "use-xdg-base-directories = true\nextra-experimental-features = nix-command flakes";
 
     nativeBuildInputs = with pkgs; [
-      # Nix toolkit
-      nix
-      nix-output-monitor
-      nix-inspect
-      deadnix
-      statix
-      home-manager
 
-      # Encryption tools/Secrets bootstrapping
+      deadnix
+      nix
+      nix-inspect
+      nix-output-monitor
+      nixfmt-rfc-style
+      statix
+
       gnupg
       openssh
-      vim # Needed for age/sops
+      vim
       sops
       ssh-to-age
       age-plugin-fido2-hmac
@@ -39,13 +36,13 @@
       age-plugin-tpm
       age-plugin-ledger
 
-      # Git setup
       gitFull # https://search.nixos.org/packages?channel=unstable&type=packages&show=gitFull
       git-extras # https://search.nixos.org/packages?channel=unstable&type=packages&show=git-extras
       tig # https://search.nixos.org/packages?channel=unstable&type=packages&show=tig
       just # https://search.nixos.org/packages?channel=unstable&type=packages&show=just
       git-credential-oauth # https://search.nixos.org/packages?channel=unstable&type=packages&show=git-credential-oauth
       git-crypt # https://search.nixos.org/packages?channel=unstable&type=packages&show=git-crypt
+
       pre-commit # https://search.nixos.org/packages?channel=unstable&type=packages&show=pre-commit
       pre-commit-hook-ensure-sops # https://search.nixos.org/packages?channel=unstable&type=packages&show=pre-commit-hook-ensure-sops
 
