635fb4203e9b2c91a63eea7f216bd0f4f6616627
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-10-23 13:54:02 +0200

    chore(nix): ephmeral staging-next
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/TP95V9LWWL/default.nix
+++ hosts/TP95V9LWWL/default.nix
@@ -367,7 +367,7 @@ in
         # https://github.com/GoldsteinE/nixos/blob/3d7353065c3f42b6442f7df9ab443fcb5381f2ce/rebuild#L13
         # https://medium.com/@zmre/nix-darwin-quick-tip-activate-your-preferences-f69942a93236
 
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff --color /run/current-system "$systemConfig"
+        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff --color=always /run/current-system "$systemConfig"
       '';
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
