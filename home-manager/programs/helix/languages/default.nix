6802e498e45177ff69c4ec973ffdb2692d9089bb
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-10-24 09:51:48 +0200

    chore(nix): edit helix
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

---
+++ home-manager/programs/helix/languages/default.nix
@@ -0,0 +1,23 @@
+_: {
+  imports = [
+    ./bash.nix
+    ./docker.nix
+    ./elixir.nix
+    ./go.nix
+    ./gpt.nix
+    ./json.nix
+    ./lua.nix
+    ./markdown.nix
+    ./nix.nix
+    ./nodejs.nix
+    ./prettier.nix
+    ./python.nix
+    ./rust.nix
+    ./template.nix
+    ./terraform.nix
+    ./toml.nix
+    ./typescript.nix
+    ./vim.nix
+    ./yaml.nix
+  ];
+}
