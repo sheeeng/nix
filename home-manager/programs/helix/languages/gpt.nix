03ba7eed5c5c89fc4309810258d3ba025cb06e7a
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-10-27 10:55:49 +0100

    chore(nix): edit helix configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/helix/languages/gpt.nix
+++ home-manager/programs/helix/languages/gpt.nix
@@ -4,12 +4,15 @@
     languages = {
       language-server = {
         gpt = {
-          command = lib.getExe pkgs.helix-gpt; # https://search.nixos.org/packages?channel=unstable&type=packages&show=helix-gpt
+          # command = lib.getExe pkgs.helix-gpt; # https://search.nixos.org/packages?channel=unstable&type=packages&show=helix-gpt
+          command = lib.getExe pkgs.bun; # https://search.nixos.org/packages?channel=unstable&type=packages&show=bun
           args = [
+            "run"
+            "/usr/local/bin/helix-gpt.js"
             "--handler"
             "copilot"
           ];
-        };
+        }; # https://github.com/helix-editor/helix/discussions/4037#discussioncomment-13972668
       };
     };
   };
