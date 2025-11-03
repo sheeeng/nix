0114cf32f8fda7c83e3143e0a92d21ca049388b2
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-11-03 13:51:10 +0100

    feat(nix): 'system' has been renamed to 'stdenv.hostPlatform.system'
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/C02ZV797MD6R/default.nix
+++ hosts/C02ZV797MD6R/default.nix
@@ -26,7 +26,7 @@ let
   inherit ((import ../core/determinate.nix { })) isDeterminateNix;
 
   pkgs-unstable = import inputs.nixpkgs {
-    inherit (hostConfiguration) system;
+    system = pkgs.stdenv.hostPlatform.system;
     config.allowUnfree = true;
     inherit (pkgs.stdenv) hostPlatform;
   };
