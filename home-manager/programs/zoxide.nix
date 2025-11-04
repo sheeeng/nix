3573f1f66e3e678b8dc1efca5521f9bd964b26d7
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-11-04 20:20:39 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/zoxide.nix
+++ home-manager/programs/zoxide.nix
@@ -2,10 +2,11 @@
 {
   programs.zoxide = {
     enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.enable
-    package = pkgs.zoxide; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.package
     enableBashIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.enableBashIntegration
     enableFishIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.enableFishIntegration
     enableNushellIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.enableNushellIntegration
     enableZshIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.enableZshIntegration
+    package = pkgs.zoxide; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.package
+    options = [ ]; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zoxide.options
   };
 }
