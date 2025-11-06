d4357bcc4079c41fe1f5bfbe94d5cf99395243ac
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-11-06 14:43:52 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -10,4 +10,7 @@
 
   # The option nixpkgs.system is still fully supported for interoperability, but will be deprecated in the future, so we recommend to set nixpkgs.hostPlatform.
   nixpkgs.hostPlatform = config.nixpkgs.system;
+
+  # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
+  fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts)
 }
