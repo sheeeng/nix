d0181c7a1a9e7be0a812bb8a3d831d415888198f
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2025-11-07 15:47:45 +0100

    chore(nix): edit configurations
    Signed-off-by: Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,19 +1,11 @@
 # _: { }
 
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ lib, pkgs, ... }:
 {
   imports = [
     # inputs.nixos-hardware.nixosModules.dell-latitude-7490
     ../../modules/nixos
   ];
-
-  # The option nixpkgs.system is still fully supported for interoperability, but will be deprecated in the future, so we recommend to set nixpkgs.hostPlatform.
-  nixpkgs.hostPlatform = config.nixpkgs.system;
 
   # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
   fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
