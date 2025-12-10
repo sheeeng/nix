4a3ba72d016c4be3bf314688ca33c91c51cc0e37
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-12-10 12:59:46 +0100

    chore(git,obsidian): ssh_auth_sock workaround
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/zsh/init-content.nix
+++ home-manager/programs/zsh/init-content.nix
@@ -11,7 +11,10 @@
 
     # 500 (mkBefore): Early initialization (replaces initExtraFirst)
     # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.initContent
-    (lib.mkOrder 500 '''')
+    # https://github.com/Vinzent03/obsidian-git/issues/959#issuecomment-3458757190
+    (lib.mkOrder 500 ''
+      export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+    '')
 
     # 550: Before completion initialization (replaces initExtraBeforeCompInit)
     # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.initContent
