2044be96ef885ecf4f3a3e1f0434d23c4433169e
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-12-15 13:09:19 +0100

    feat(1password): install shell plugins
    See https://developer.1password.com/docs/cli/shell-plugins/nix.
    
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/_1password-shell-plugins.nix
+++ home-manager/programs/_1password-shell-plugins.nix
@@ -4,13 +4,13 @@
   imports = [ inputs._1password-shell-plugins.hmModules.default ];
 
   programs._1password-shell-plugins = {
-    enable = true;
+    enable = true; # https://github.com/1Password/shell-plugins/blob/49810df8fe11221250a890191185c6e59aea8d1e/nix/shell-plugins.nix#L29
     plugins = with pkgs; [
       # keep-sorted start
       awscli2 # https://search.nixos.org/packages?channel=unstable&type=packages&show=awscli2
       cachix # https://search.nixos.org/packages?channel=unstable&type=packages&show=cachix
       gh # https://search.nixos.org/packages?channel=unstable&type=packages&show=gh
       # keep-sorted end
-    ];
+    ]; # https://github.com/1Password/shell-plugins/blob/49810df8fe11221250a890191185c6e59aea8d1e/nix/shell-plugins.nix#L31
   };
 }
