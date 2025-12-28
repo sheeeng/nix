09e9d9a558083a5dc561cea0f9e312766692435a
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2025-12-28 19:45:12 +0100

    chore: purge unnecessary files
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- archive/nixvim/plugins/plugins-b.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  programs.nixvim.plugins = {
-    bufferline = {
-      enable = true; # https://nix-community.github.io/nixvim/plugins/bufferline/index.html#pluginsbufferlineenable
-    }; # https://nix-community.github.io/nixvim/plugins/bufferline/index.html
-  };
-}
