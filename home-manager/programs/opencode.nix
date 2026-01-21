1c6265212363c12c72041046522e5761363141eb
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-21 19:34:31 +0100

    chore(opencode): edit settings
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

---
+++ home-manager/programs/opencode.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+  programs.opencode = {
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.enable
+    enableMcpIntegration = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.enableMcpIntegration
+    package = pkgs.opencode; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.package
+    agents = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.agents
+    commands = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.commands
+    rules = ""; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.rules
+    settings = {
+      theme = "opencode";
+      model = "anthropic/claude-sonnet-4-5";
+      small_model = "anthropic/claude-haiku-4-5";
+      autoshare = false;
+      autoupdate = true;
+      instructions = [
+        "./github/copilot-instructions.md"
+        ".cursor/rules/*.md"
+        "AGENTS.md"
+      ];
+
+    }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.settings
+    skills = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.skills
+    themes = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.themes
+    tools = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.tools
+  };
+}
