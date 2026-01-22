9bf1024a52a5f850c21306191fc7332625f4caae
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-22 13:32:16 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

---
+++ home-manager/programs/aider-chat.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  programs.aider-chat = {
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.aider-chat.enable
+    package = pkgs.aider-chat; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.aider-chat.package
+    settings = {
+      architect = false;
+      auto-accept-architect = true;
+      cache-prompts = false;
+      lint = true;
+      show-model-warnings = true;
+      verify-ssl = true;
+    }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.aider-chat.settings
+  };
+}
