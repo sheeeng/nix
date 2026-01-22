a23f972b680cd1711fd96c82212ac37ec323a076
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-22 19:20:20 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/rust.nix
+++ home-manager/packages/rust.nix
@@ -9,45 +9,50 @@
 let
   cargoHome = "${config.xdg.dataHome}/.cargo";
 in
-lib.mkIf (pkgs.stdenv.hostPlatform.system != "x86_64-darwin") {
-  home.packages = [
-    # Use individual components instead of the full toolchain to avoid conflicts
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.cargo
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.clippy
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.llvm-tools
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.miri
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-analysis
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-docs
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-src
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc-codegen-cranelift
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc-dev
-    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustfmt
-    # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.toolchain # Disabled to avoid rust-analyzer conflicts
-
-    pkgs.cabal-install # https://search.nixos.org/packages?channel=unstable&type=packages&show=cabal-install
-    pkgs.ghc # https://search.nixos.org/packages?channel=unstable&type=packages&show=ghc
-    pkgs.libiconv # https://search.nixos.org/packages?channel=unstable&type=packages&show=libiconv
-    pkgs.rust-analyzer # TODO: # Use nixpkgs rust-analyzer to avoid apple_sdk_11_0 issue. # https://search.nixos.org/packages?channel=unstable&type=packages&show=rust-analyzer
-    # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-analyzer # Temporarily disabled due to apple_sdk_11_0 issue
-    # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.rust-analyzer-vscode-extension # Temporarily disabled due to apple_sdk_11_0 issue
-  ];
-
-  home.sessionVariables = {
-    RUSTUP_HOME = "${config.xdg.dataHome}/.rustup"; # https://rust-lang.github.io/rustup/configuration.html
-    CARGO_HOME = cargoHome;
-    RUST_SRC_PATH = "${
+lib.mkIf
+  (
+    pkgs.stdenv.hostPlatform.system != "x86_64-darwin"
+    && pkgs.stdenv.hostPlatform.system != "x86_64-linux"
+  )
+  {
+    home.packages = [
+      # Use individual components instead of the full toolchain to avoid conflicts
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.cargo
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.clippy
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.llvm-tools
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.miri
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-analysis
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-docs
       inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-src
-    }/lib/rustlib/src/rust/library";
-  };
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc-codegen-cranelift
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustc-dev
+      inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rustfmt
+      # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.toolchain # Disabled to avoid rust-analyzer conflicts
 
-  home.sessionPath = [ "${cargoHome}/bin" ];
+      pkgs.cabal-install # https://search.nixos.org/packages?channel=unstable&type=packages&show=cabal-install
+      pkgs.ghc # https://search.nixos.org/packages?channel=unstable&type=packages&show=ghc
+      pkgs.libiconv # https://search.nixos.org/packages?channel=unstable&type=packages&show=libiconv
+      pkgs.rust-analyzer # TODO: # Use nixpkgs rust-analyzer to avoid apple_sdk_11_0 issue. # https://search.nixos.org/packages?channel=unstable&type=packages&show=rust-analyzer
+      # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-analyzer # Temporarily disabled due to apple_sdk_11_0 issue
+      # inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.rust-analyzer-vscode-extension # Temporarily disabled due to apple_sdk_11_0 issue
+    ];
 
-  home.file."${cargoHome}/config.toml".text = ''
-    [target.aarch64-apple-darwin]
-    rustflags = "-L${pkgs.libiconv}/lib";
+    home.sessionVariables = {
+      RUSTUP_HOME = "${config.xdg.dataHome}/.rustup"; # https://rust-lang.github.io/rustup/configuration.html
+      CARGO_HOME = cargoHome;
+      RUST_SRC_PATH = "${
+        inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.latest.rust-src
+      }/lib/rustlib/src/rust/library";
+    };
 
-    [target.x86_64-apple-darwin]
-    rustflags = "-L${pkgs.libiconv}/lib";
-  '';
-}
+    home.sessionPath = [ "${cargoHome}/bin" ];
+
+    home.file."${cargoHome}/config.toml".text = ''
+      [target.aarch64-apple-darwin]
+      rustflags = "-L${pkgs.libiconv}/lib";
+
+      [target.x86_64-apple-darwin]
+      rustflags = "-L${pkgs.libiconv}/lib";
+    '';
+  }
