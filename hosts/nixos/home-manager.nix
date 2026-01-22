25e29d4091c5f34169f6aaf081859e4e366f43bd
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-22 18:29:27 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/nixos/home-manager.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  imports = [ ../../home-manager/home.nix ];
-
-  # Add any laptop-specific home-manager overrides here
-  # For example:
-  # programs.kitty.settings.font_size = 11;
-}
