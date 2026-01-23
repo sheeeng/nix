62cca99240e15f2fc086d729c1c91f0ef5db421b
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-23 16:36:16 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- treefmt.nix
+++ treefmt.nix
@@ -1,5 +1,3 @@
-# https://github.com/rosenpass/rosenpass/blob/cdf6e8369f329b9c535c113d5e146dc3d2b1f320/treefmt.nix
-
 { pkgs, ... }:
 {
   projectRootFile = "flake.nix";
@@ -7,24 +5,16 @@
   programs.mdsh.enable = true;
 
   programs = {
+    # keep-sorted start block=yes newline_separated=no case=no sticky_comments=yes
+    deadnix.enable = true;
+    just.enable = true;
+    keep-sorted.enable = true;
     nixfmt = {
       enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
       package = pkgs.nixfmt-rfc-style;
       includes = [ "*.nix" ];
       strict = true;
     };
-
-    shfmt = {
-      enable = true;
-      indent_size = 2;
-    };
-    shellcheck = {
-      enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
-    };
-
-    deadnix.enable = true;
-    statix.enable = true;
-
     prettier = {
       enable = true;
       includes = [
@@ -51,26 +41,49 @@
         useTabs = false;
       };
     };
-
-    yamlfmt = {
-      enable = true;
-      settings = {
-        formatter = {
-          retain_line_breaks_single = true;
-        };
-      };
+    rustfmt.enable = true;
+    shellcheck = {
+      enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
+      includes = [
+        "*.bash"
+        "*.bats"
+        "*.sh"
+      ];
     };
-
+    shfmt = {
+      enable = true;
+      indent_size = 2;
+      includes = [
+        "*.bash"
+        "*.bats"
+        "*.sh"
+      ];
+    };
+    statix.enable = true;
     taplo = {
       enable = true;
       settings = {
         include = [ "*.toml" ];
       };
     };
-
-    just.enable = true;
-
-    rustfmt.enable = true;
+    yamlfmt = {
+      enable = true;
+      settings = {
+        formatter = {
+          disallow_anchors = false;
+          eof_newline = true;
+          include_document_start = false;
+          indent = 2;
+          line_endings = "lf";
+          max_line_length = 0;
+          retain_line_breaks = true;
+          retain_line_breaks_single = true;
+          scan_folded_as_literal = true;
+          trim_trailing_whitespace = true;
+        };
+      };
+    };
+    # keep-sorted end
   };
 
   settings = {
@@ -81,5 +94,47 @@
       "archive/**"
       ".github/**/*.md"
     ];
+  };
+
+  settings.formatter = {
+    # keep-sorted start block=yes newline_separated=no case=no sticky_comments=yes
+    keep-sorted = {
+      options = [
+        "--mode"
+        "fix"
+      ];
+    };
+    shfmt = {
+      options = [
+        "--indent"
+        "2"
+        "--space-redirects"
+        "--binary-next-line"
+        "--diff"
+        "--simplify"
+        "--list"
+        "--write"
+      ];
+    };
+    # Using opentofu as the formatter for Terraform files, as official terraform binaries are not pre-built due to restrictive license.
+    # Building terraform from source is not time-efficient for CI runs.
+    terraform = {
+      command = "${pkgs.opentofu}/bin/tofu"; # https://search.nixos.org/packages?channel=unstable&type=packages&show=opentofu
+      options = [ "fmt" ];
+      includes = [
+        "*.tf"
+        "*.tfvars"
+        "*.tftest.hcl"
+      ];
+      excludes = [ ];
+    };
+    yamllint = {
+      command = "${pkgs.yamllint}/bin/yamllint";
+      includes = [
+        "*.yaml"
+        "*.yml"
+      ];
+    };
+    # keep-sorted end
   };
 }
