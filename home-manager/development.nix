68eb5a3b71dc95c86b5f99d5cdfa63e9c0348470
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-25 11:35:22 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/development.nix
+++ home-manager/development.nix
@@ -2,17 +2,19 @@
 
 # https://github.com/hardselius/dotfiles/blob/b801fd8aba017a588ce56430d8345449ec396c96/home/packages.nix
 
-{ pkgs, ... }:
-{
-  home.packages =
-    with pkgs;
-    [
-      code-cursor # https://search.nixos.org/packages?channel=unstable&type=packages&show=code-cursor
-      # claude-code # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=claude-code
-    ]
-    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ ])
-    ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-      vscodium # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=vscodium
-    ])
-    ++ (pkgs.lib.optionals (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) [ ]);
-}
+_: {}
+
+# { pkgs, ... }:
+# {
+#   home.packages =
+#     with pkgs;
+#     [
+#       code-cursor # https://search.nixos.org/packages?channel=unstable&type=packages&show=code-cursor
+#       # claude-code # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=claude-code
+#     ]
+#     ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ ])
+#     ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+#       vscodium # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=vscodium
+#     ])
+#     ++ (pkgs.lib.optionals (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) [ ]);
+# }
