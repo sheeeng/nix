a912d35a18a179deaa692ad8ede5e8eaea463e79
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-26 21:45:48 +0100

    chore(nix): edit overlays
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -58,6 +58,19 @@ inputs: {
   # nix eval nixpkgs#beads --json 2>&1 | head -20
   # nix eval .#darwinConfigurations.$(hostname).pkgs.beads.version --raw 2>&1
   # nix build .#darwinConfigurations.$(hostname).pkgs.beads --print-build-logs 2>&1
+  # To update source hash:
+  # nix-prefetch-git \
+  #   https://github.com/steveyegge/beads \
+  #   0d99d15370030b953a8df0ea67cd3d1b845bb07b \
+  #   | jq --raw-output '.hash'
+  # To update vendorHash (recommended):
+  # 1. Set: vendorHash = final.lib.fakeHash;
+  # 2. Run: nix build .#darwinConfigurations.$(hostname).pkgs.beads
+  # 3. Copy hash from error message
+  # Alternative vendorHash method (may not work):
+  # nix --extra-experimental-features 'nix-command flakes' run nixpkgs#nix-prefetch -- \
+  #   '{ sha256 }: (builtins.getFlake "git+file://'$(pwd)'").darwinConfigurations.'$(hostname)'.pkgs.beads.goModules.overrideAttrs (_: { outputHash = sha256; })'
+  # TODO: https://github.com/NixOS/nixpkgs/pull/483469
   beads = final: prev: {
     beads = prev.beads.overrideAttrs (old: rec {
       version = "0d99d15370030b953a8df0ea67cd3d1b845bb07b"; # v0.49.1
@@ -67,12 +80,9 @@ inputs: {
         rev = version;
         hash = "sha256-roOyTMy9nKxH2Bk8MnP4h2CDjStwK6z0ThQhFcM64QI=";
       };
+      vendorHash = "sha256-YU+bRLVlWtHzJ1QPzcKJ70f+ynp8lMoIeFlm+29BNPE=";
     });
   };
-  # nix-prefetch-git \
-  #   https://github.com/steveyegge/beads \
-  #   0d99d15370030b953a8df0ea67cd3d1b845bb07b \
-  #   | jq --raw-output '.hash'
 
   # --------------------------------------------------------------------
 
