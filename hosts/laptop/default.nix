3b33e2c3c179068cda096efc2e07bb1f79b80d1f
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-27 21:09:10 +0100

    chore: comment lines
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,23 +1,23 @@
-# _: { }
+_: { }
 
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  imports = [
-    inputs.nixos-hardware.nixosModules.dell-latitude-7490
-    ../../modules/nixos
-  ];
+# {
+#   inputs,
+#   lib,
+#   pkgs,
+#   ...
+# }:
+# {
+#   imports = [
+#     inputs.nixos-hardware.nixosModules.dell-latitude-7490
+#     ../../modules/nixos
+#   ];
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-    systemd-boot.configurationLimit = 10;
-  };
+#   boot.loader = {
+#     systemd-boot.enable = true;
+#     efi.canTouchEfiVariables = true;
+#     systemd-boot.configurationLimit = 10;
+#   };
 
-  # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
-  fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-}
+#   # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
+#   fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+# }
