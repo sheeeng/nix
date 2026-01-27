ca097082c333e75a65864ac07849d45b25009c62
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-27 21:09:10 +0100

    chore: comment lines
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -1,23 +1,23 @@
-# _: { }
+_: { }
 
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  imports = [
-    inputs.nixos-hardware.nixosModules.raspberry-pi-4
-    ../../modules/nixos
-  ];
+# {
+#   inputs,
+#   lib,
+#   pkgs,
+#   ...
+# }:
+# {
+#   imports = [
+#     inputs.nixos-hardware.nixosModules.raspberry-pi-4
+#     ../../modules/nixos
+#   ];
 
-  boot.loader = {
-    generic-extlinux-compatible.enable = true;
-    efi.canTouchEfiVariables = true;
-    systemd-boot.configurationLimit = 10;
-  };
+#   boot.loader = {
+#     generic-extlinux-compatible.enable = true;
+#     efi.canTouchEfiVariables = true;
+#     systemd-boot.configurationLimit = 10;
+#   };
 
-  # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
-  fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-}
+#   # https://wiki.nixos.org/wiki/fonts#installing_all_nerdfonts
+#   fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+# }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,104 +1,106 @@
-{
-  pkgs,
-  inputs,
-  ...
-}:
+_: { }
 
-let
-  pkgs-unstable = import inputs.nixpkgs {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-  };
-in
-{
-  imports = [
-    # (modulesPath + "/installer/scan/not-detected.nix")
-    inputs.home-manager.nixosModules.default
-  ];
+# {
+#   pkgs,
+#   inputs,
+#   ...
+# }:
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+# let
+#   pkgs-unstable = import inputs.nixpkgs {
+#     inherit (pkgs.stdenv.hostPlatform) system;
+#     config.allowUnfree = true;
+#   };
+# in
+# {
+#   imports = [
+#     # (modulesPath + "/installer/scan/not-detected.nix")
+#     inputs.home-manager.nixosModules.default
+#   ];
 
-  boot.initrd.luks.devices = {
-    root = {
-      device = "/dev/nvme0n1p2";
-      preLVM = true;
-      allowDiscards = true;
-    };
-  };
+#   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/nvme0n1p1";
-      fsType = "vfat";
-    };
-    "/" = {
-      device = "/dev/vg/root";
-      fsType = "ext4";
-    };
-    "/home" = {
-      device = "/dev/vg/home";
-      fsType = "ext4";
-    };
-  };
+#   boot.initrd.luks.devices = {
+#     root = {
+#       device = "/dev/nvme0n1p2";
+#       preLVM = true;
+#       allowDiscards = true;
+#     };
+#   };
 
-  swapDevices = [ ];
+#   fileSystems = {
+#     "/boot" = {
+#       device = "/dev/nvme0n1p1";
+#       fsType = "vfat";
+#     };
+#     "/" = {
+#       device = "/dev/vg/root";
+#       fsType = "ext4";
+#     };
+#     "/home" = {
+#       device = "/dev/vg/home";
+#       fsType = "ext4";
+#     };
+#   };
 
-  nixpkgs.config.allowUnfree = true;
+#   swapDevices = [ ];
 
-  system.stateVersion = "24.11";
-  nix = {
-    optimise.automatic = true; # https://daiderd.com/nix-darwin/manual/index.html#opt-nix.optimise.automatic # https://github.com/NixOS/nix/issues/7273#issuecomment-2295429401
-    settings = {
-      auto-optimise-store = true; # https://github.com/NixOS/nix/issues/7273#issuecomment-1310213986
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      substituters = [ "https://cache.nixos.org" ];
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-      extra-substituters = [
-        "https://devenv.cachix.org"
-        "https://ryanccn.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "ryanccn.cachix.org-1:Or82F8DeVLJgjSKCaZmBzbSOhnHj82Of0bGeRniUgLQ="
-      ];
-    };
-  };
+#   nixpkgs.config.allowUnfree = true;
 
-  services.lvm.enable = true;
+#   system.stateVersion = "24.11";
+#   nix = {
+#     optimise.automatic = true; # https://daiderd.com/nix-darwin/manual/index.html#opt-nix.optimise.automatic # https://github.com/NixOS/nix/issues/7273#issuecomment-2295429401
+#     settings = {
+#       auto-optimise-store = true; # https://github.com/NixOS/nix/issues/7273#issuecomment-1310213986
+#       experimental-features = [
+#         "nix-command"
+#         "flakes"
+#       ];
+#       substituters = [ "https://cache.nixos.org" ];
+#       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+#       extra-substituters = [
+#         "https://devenv.cachix.org"
+#         "https://ryanccn.cachix.org"
+#       ];
+#       extra-trusted-public-keys = [
+#         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+#         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+#         "ryanccn.cachix.org-1:Or82F8DeVLJgjSKCaZmBzbSOhnHj82Of0bGeRniUgLQ="
+#       ];
+#     };
+#   };
 
-  networking = {
-    useNetworkd = true;
-    wireless.iwd.enable = true;
-    interfaces.wlan0.useDHCP = true;
-    firewall.enable = true;
-  };
+#   services.lvm.enable = true;
 
-  time.timeZone = "Europe/Oslo";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = "nb_NO.UTF-8";
-    };
-  };
+#   networking = {
+#     useNetworkd = true;
+#     wireless.iwd.enable = true;
+#     interfaces.wlan0.useDHCP = true;
+#     firewall.enable = true;
+#   };
 
-  security.polkit.enable = true;
+#   time.timeZone = "Europe/Oslo";
+#   i18n = {
+#     defaultLocale = "en_US.UTF-8";
+#     extraLocaleSettings = {
+#       LC_TIME = "nb_NO.UTF-8";
+#     };
+#   };
 
-  # https://nixos.wiki/wiki/Bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+#   security.polkit.enable = true;
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
+#   # https://nixos.wiki/wiki/Bluetooth
+#   hardware.bluetooth.enable = true; # enables support for Bluetooth
+#   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  home-manager.users.leonard = ./home-manager.nix;
+#   home-manager.useGlobalPkgs = true;
+#   home-manager.useUserPackages = true;
+#   home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
 
-  users.users.leonard = {
-    group = "wheel";
-    isSystemUser = true;
-  };
-}
+#   home-manager.users.leonard = ./home-manager.nix;
+
+#   users.users.leonard = {
+#     group = "wheel";
+#     isSystemUser = true;
+#   };
+# }
