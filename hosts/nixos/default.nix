a44c4df7efd8eae749b3195ef825e6d5a90a3c4e
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-27 13:57:06 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -191,17 +191,23 @@ in
         "flakes"
       ];
       substituters = [
-        "https://cache.nixos.org"
+        "https://cache.nixos.org?priority=10" # curl https://cache.nixos.org/nix-cache-info
         "https://devenv.cachix.org"
         "https://nixpkgs-python.cachix.org"
         "https://ryanccn.cachix.org"
+        "https://nix-community.cachix.org?priority=20" # curl https://nix-community.cachix.org/nix-cache-info
+        "https://nix-gaming.cachix.org"
+        "https://nix-citizen.cachix.org"
       ];
       trusted-public-keys = [
+        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
         "ryanccn.cachix.org-1:Or82F8DeVLJgjSKCaZmBzbSOhnHj82Of0bGeRniUgLQ="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
       ];
       trusted-substituters = [ "https://hydra.nixos.org/" ];
       trusted-users = [
