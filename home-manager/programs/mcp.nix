a9b2d6b4b9ab4c449b04c76f3671eecb5cf9bc98
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-29 14:22:33 +0100

    chore(mcp): edit settings
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

---
+++ home-manager/programs/mcp.nix
@@ -0,0 +1,27 @@
+{
+  programs.mcp = {
+    enable = true;
+    servers = {
+      context7 = {
+        url = "https://mcp.context7.com/mcp";
+        headers = {
+          CONTEXT7_API_KEY = "{env:CONTEXT7_API_KEY}";
+        };
+      };
+      github = {
+        command = "docker";
+        args = [
+          "run"
+          "-i"
+          "--rm"
+          "-e"
+          "GITHUB_PERSONAL_ACCESS_TOKEN"
+          "ghcr.io/github/github-mcp-server"
+        ];
+        env = {
+          GITHUB_PERSONAL_ACCESS_TOKEN = "{env:GITHUB_TOKEN}";
+        };
+      };
+    };
+  }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mcp.servers
+}
