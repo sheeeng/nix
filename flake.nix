1b6275e36d57e789996944d36e61d824575e3bdb
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-01-31 18:30:54 +0100

    chore(nix): flake update
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -242,7 +242,7 @@
 
     beads-flake = {
       # # TODO: Undo when https://github.com/steveyegge/beads/issues/1373#issuecomment-3815509770 is fixed.
-      url = "github:steveyegge/beads/v0.49.2";
+      url = "github:steveyegge/beads/v0.49.1"; # TODO: See https://github.com/steveyegge/beads/issues/1373 issue.
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
