1e900d655a8b9375907e07f751fa54fb1fcc11d3
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-03 21:50:13 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/scripts.nix
+++ home-manager/scripts.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
 
-  shellHeader = ''
+  shellScriptHeader = ''
     #!${pkgs.stdenv.shell}
 
     ## https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
@@ -13,48 +18,76 @@ let
     ## https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
     shopt -s inherit_errexit # If set, command substitution inherits the value of the errexit option, instead of unsetting it in the subshell environment. This option is enabled when POSIX mode is enabled.
 
-    echo $(date --universal --iso-8601=seconds)
+    echo $(${lib.getExe' pkgs.coreutils "date"} --universal --iso-8601=seconds)
 
   '';
 
   #  https://github.com/legendofmiracles/dotnix/blob/8dfa01af04d6391a1f5cb2c788bdecc1ee748ca9/hm/shell-scripts.nix
 
-  my-build-live-iso = pkgs.writeScriptBin "my-build-live-iso" (
-    "${shellHeader}"
+  build-my-live-iso = pkgs.writeScriptBin "build-my-live-iso" (
+    "${shellScriptHeader}"
     + ''
       cd /tmp
       nix build /etc/nixos#nixosConfigurations.live-usb.config.system.build.isoImage
     ''
   );
 
-  my-host-info = pkgs.writeScriptBin "my-host-info" (
-    "${shellHeader}"
+  show-my-host = pkgs.writeScriptBin "show-my-host" (
+    "${shellScriptHeader}"
     + ''
-      LOCAL_IP=$(ip -o addr show | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $4}' | cut -d'/' -f 1)
-      PUBLIC_IP=$(curl -s ifconfig.me)
-      CPU=$(sudo lshw -short | grep -i processor | sed 's/\s\s*/ /g' | cut -d' ' -f3-)
-      VIDEO=$(sudo lspci | grep -i --color 'vga\|3d\|2d' | cut -d' ' -f2-)
+      OS_TYPE=$(uname -s)
+      PUBLIC_IP=$(${lib.getExe pkgs.curl} --silent ifconfig.me)
 
-      echo -e "local: $LOCAL_IP, public: $PUBLIC_IP\n"
-      echo -e "Processor: $CPU"
-      echo -e "Video: $VIDEO\n"
+      if [ "$OS_TYPE" = "Darwin" ]; then
+        # macOS
+        LOCAL_IP=$(ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | head -1)
+        CPU=$(sysctl -n machdep.cpu.brand_string)
+        VIDEO=$(system_profiler SPDisplaysDataType 2>/dev/null | grep "Chipset Model" | sed 's/.*: //' | head -1)
 
-      echo -e "\n"
-      lsblk -f
+        echo -e "Local: $LOCAL_IP, Public: $PUBLIC_IP\n"
+        echo -e "Processor: $CPU"
+        echo -e "Video: $VIDEO\n"
 
-      echo -e "\n"
-      lsmod | rg kvm
+        echo -e "\nDisk Info:"
+        diskutil list
+
+        echo -e "\nLoaded Kernel Extensions (kexts):"
+        kextstat | head -20
+      else
+        # Linux
+        LOCAL_IP=$(${
+          if pkgs.stdenv.isLinux then lib.getExe' pkgs.iproute2 "ip" else "ip"
+        } -o addr show | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $4}' | cut -d'/' -f 1)
+        CPU=$(sudo ${
+          if pkgs.stdenv.isLinux then lib.getExe pkgs.lshw else "lshw"
+        } -short 2>/dev/null | grep -i processor | sed 's/\s\s*/ /g' | cut -d' ' -f3-)
+        VIDEO=$(${
+          if pkgs.stdenv.isLinux then lib.getExe' pkgs.pciutils "lspci" else "lspci"
+        } | grep -i 'vga\|3d\|2d' | cut -d' ' -f2-)
+
+        echo -e "Local: $LOCAL_IP, Public: $PUBLIC_IP\n"
+        echo -e "Processor: $CPU"
+        echo -e "Video: $VIDEO\n"
+
+        echo -e "\nDisk Info:"
+        ${if pkgs.stdenv.isLinux then lib.getExe' pkgs.util-linux "lsblk" else "lsblk"} -f
+
+        echo -e "\nKernel Modules (KVM):"
+        ${
+          if pkgs.stdenv.isLinux then lib.getExe' pkgs.kmod "lsmod" else "lsmod"
+        } | ${lib.getExe pkgs.ripgrep} kvm || echo "No KVM modules loaded"
+      fi
     ''
   );
 
-  my-tmux = pkgs.writeScriptBin "my-tmux" (
-    "${shellHeader}"
+  run-my-tmux = pkgs.writeScriptBin "run-my-tmux" (
+    "${shellScriptHeader}"
     + ''
       if [ -z "$1" ]
         then
-          tmux new -A -s 🦙
+          ${lib.getExe pkgs.tmux} new -A -s 🦙
         else
-          tmux new -A -s $1
+          ${lib.getExe pkgs.tmux} new -A -s $1
       fi
     ''
   );
@@ -62,31 +95,31 @@ in
 {
   # https://discourse.nixos.org/t/how-to-import-into-main-home-nix/55289/2
   home.packages = [
-    my-build-live-iso
-    my-host-info
-    my-tmux
+    build-my-live-iso
+    show-my-host
+    run-my-tmux
 
-    (pkgs.writeShellScriptBin "my-hello" (
-      "${shellHeader}"
+    (pkgs.writeShellScriptBin "say-my-hello" (
+      "${shellScriptHeader}"
       + ''
         echo "Hello, ${config.home.username}!"
       ''
     ))
 
-    (pkgs.writeShellScriptBin "my-cowsay" (
-      "${shellHeader}"
+    (pkgs.writeShellScriptBin "run-my-cowsay" (
+      "${shellScriptHeader}"
       + ''
         echo ''${PATH} | tr ':' '\n' \
-          | ${pkgs.cowsay}/bin/cowsay -n -f small \
-          | ${pkgs.lolcat}/bin/lolcat --animate --duration 1 --speed 512 --truecolor
+          | ${lib.getExe pkgs.cowsay} -n -f small \
+          | ${lib.getExe pkgs.lolcat} --animate --duration 1 --speed 512 --truecolor
         echo '----------------'
         echo \$\{PATH\} Items: $(echo ''${PATH} | tr ':' '\n' | wc --lines)
         echo '----------------'
       ''
     ))
 
-    (pkgs.writeShellScriptBin "my-path" (
-      "${shellHeader}"
+    (pkgs.writeShellScriptBin "show-my-path" (
+      "${shellScriptHeader}"
       + ''
         echo \$\{PATH\} Items: $(echo ''${PATH} | tr ':' '\n' | wc --lines)
         echo '----------------'
@@ -95,8 +128,8 @@ in
       ''
     ))
 
-    (pkgs.writeShellScriptBin "my-colors" (
-      "${shellHeader}"
+    (pkgs.writeShellScriptBin "show-my-colors" (
+      "${shellScriptHeader}"
       + ''
         # https://unix.stackexchange.com/questions/60968/tmux-bottom-status-bar-color-change/60969#60969
         # https://unix.stackexchange.com/a/60969
@@ -108,15 +141,15 @@ in
     ))
 
     # Generate a UUID v5 (SHA-1 based) from a string argument.
-    (pkgs.writeShellScriptBin "my-uuid" (
-      "${shellHeader}"
+    (pkgs.writeShellScriptBin "generate-my-uuid" (
+      "${shellScriptHeader}"
       + ''
         if [ -z "''${1:-}" ]; then
-          echo "Usage: my-uuid <string>"
+          echo "Usage: generate-my-uuid <string>"
           echo "Generates a UUID v5 (SHA-1 based) from the input string using OID namespace."
           exit 1
         fi
-        ${pkgs.util-linux}/bin/uuidgen --sha1 --namespace @oid --name "$1"
+        ${lib.getExe' pkgs.util-linux "uuidgen"} --sha1 --namespace @oid --name "$1"
       ''
     ))
   ];
