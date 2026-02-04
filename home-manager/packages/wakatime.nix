7d58f88c88f0e232ab73341556750149afe077da
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-04 16:36:04 +0100

    chore(nix): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/wakatime.nix
+++ home-manager/packages/wakatime.nix
@@ -1,19 +1,28 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    # wakatime-cli # https://search.nixos.org/packages?channel=unstable&type=packages&show=wakatime-cli
-  ];
+_: { }
 
-  home.sessionVariables = {
-    # TODO: WAKATIME_API_KEY
-  };
+# {
+#   config,
+#   lib,
+#   pkgs,
+#   ...
+# }:
+# {
+#   home.packages = with pkgs; [
+#     wakatime-cli # https://search.nixos.org/packages?channel=unstable&type=packages&show=wakatime-cli
+#   ];
 
-  home.file = {
-    ".wakatime.cfg" = {
-      text = ''
-        [settings]
-        # TODO: api_key =
-      '';
-    };
-  };
-}
+#   home.sessionVariables = {
+#     WAKATIME_API_KEY_FILE = config.sops.secrets."keys/wakatime".path;
+#   };
+
+#   home.activation.setupWakaTimeConfiguration = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+#     # If the script block produces any observable side effect, such as writing or deleting files, then it must be placed after the special writeBoundary script block.
+#     # https://nix-community.github.io/home-manager/options.xhtml#opt-home.activation
+#     # The script runs after `writeBoundary`, ensuring the secret is already decrypted and available to read.
+#     $DRY_RUN_CMD mkdir --parents $VERBOSE_ARG ${config.home.homeDirectory}/.config/wakatime || true
+#     if [ -f ${config.sops.secrets."keys/wakatime".path} ]; then
+#       API_KEY=$(cat ${config.sops.secrets."keys/wakatime".path})
+#       printf "[settings]\napi_key = %s\n" "$API_KEY" > ${config.home.homeDirectory}/.wakatime.cfg
+#     fi
+#   '';
+# }
