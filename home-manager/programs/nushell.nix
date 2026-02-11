63da4be9090fd9719de75374a96f2a5cb6d3c208
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-11 13:12:44 +0100

    chore(nushell): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -2,7 +2,7 @@
 {
   programs.nushell = {
     # @upstream-issue https://github.com/NixOS/nixpkgs/issues/485915
-    enable = !pkgs.stdenv.isDarwin; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.enable
+    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.enable
     package = pkgs.nushell; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.package
     configFile = null; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.configFile
     envFile = null; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.envFile
