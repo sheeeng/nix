7b5f47f95eba7cc0eee2264611bec668053f353f
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-16 20:36:07 +0100

    feat(home-manager): enable element-desktop for linux
    Enable Element Desktop using programs.element-desktop on Linux hosts only.
    This provides better integration and configuration management compared to
    installing as a package.

--- home-manager/programs/element-desktop.nix
+++ home-manager/programs/element-desktop.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.element-desktop = {
-    enable = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.element-desktop.enable
+    enable = pkgs.stdenv.isLinux; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.element-desktop.enable
     package = pkgs.element-desktop; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.element-desktop.package
     profiles = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.element-desktop.profiles
     settings = { }; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.element-des
