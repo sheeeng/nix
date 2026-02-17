94e6b5f612315686367da3b2912d4132cbdbc185
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-17 20:06:57 +0100

    chore(opencode): edit settings
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/opencode/default.nix
+++ home-manager/programs/opencode/default.nix
@@ -6,11 +6,11 @@
     package = pkgs.opencode; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.package
     agents = {
       # https://opencode.ai/docs/agents/#markdown
-      build = ./agents/builder.md;
+      # build = ./agents/builder.md;
       builder = ./agents/builder.md;
       code-reviewer = ./agents/code-reviewer.md;
       explorer = ./agents/explorer.md;
-      plan = ./agents/planner.md;
+      # plan = ./agents/planner.md;
       planner = ./agents/planner.md;
       security-auditor = ./agents/security-auditor.md;
       technical-writer = ./agents/technical-writer.md;
