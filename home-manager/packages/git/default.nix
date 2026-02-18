9b5aec931f859abd97b16ef202f3f08e0e1408b9
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-18 10:45:54 +0100

    chore(git): edit configuration
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/git/default.nix
+++ home-manager/packages/git/default.nix
@@ -157,6 +157,8 @@ in
           lgfs = "log --format=fuller --show-signature";
           lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
           lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+          log50 = "log --format='%C(auto)%h %C(green)(%<(4)%gd)%Creset %<(50,trunc)%s %C(dim)(%cr)%Creset'";
+          log72 = "log --format='%C(auto)%h %C(green)(%<(4)%gd)%Creset %<(72,trunc)%s %C(dim)(%cr)%Creset'";
           lol = "log --oneline --decorate --graph --all";
           root = "rev-parse --show-toplevel";
           unstage = "reset HEAD --";
