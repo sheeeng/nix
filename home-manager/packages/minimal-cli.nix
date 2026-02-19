521962ada5e676c197b4568141ec7170ba2c4963
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-02-19 10:25:56 +0100

    chore(nix): edit packages
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages/minimal-cli.nix
+++ home-manager/packages/minimal-cli.nix
@@ -4,50 +4,58 @@
 }:
 {
   home.packages = with pkgs; [
-    # Basic utilities
+    # Basic Utilities
     # keep-sorted start block=no newline_separated=no
     (lib.hiPrio uutils-coreutils-noprefix) # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-coreutils-noprefixd
     bc # https://search.nixos.org/packages?channel=unstable&type=packages&show=bc
     curl # https://search.nixos.org/packages?channel=unstable&type=packages&show=curl
     dos2unix # https://search.nixos.org/packages?channel=unstable&type=packages&show=dos2unix
-    fd # https://search.nixos.org/packages?channel=unstable&type=packages&show=fd
+    editorconfig-checker # https://search.nixos.org/packages?channel=unstable&type=packages&show=editorconfig-checker
     findutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=findutils
-    fzf # https://search.nixos.org/packages?channel=unstable&type=packages&show=fzf
     gzip # https://search.nixos.org/packages?channel=unstable&type=packages&show=gzip
     jq # https://search.nixos.org/packages?channel=unstable&type=packages&show=jq
     less # https://search.nixos.org/packages?channel=unstable&type=packages&show=less
-    ripgrep # https://search.nixos.org/packages?channel=unstable&type=packages&show=ripgrep
-    starship # https://search.nixos.org/packages?channel=unstable&type=packages&show=starship
+    moor # https://search.nixos.org/packages?channel=unstable&type=packages&show=moor
+    timer # https://search.nixos.org/packages?channel=unstable&type=packages&show=timer
     unzip # https://search.nixos.org/packages?channel=unstable&type=packages&show=unzip
     vim # https://search.nixos.org/packages?channel=unstable&type=packages&show=vim
     wget # https://search.nixos.org/packages?channel=unstable&type=packages&show=wget
-    zoxide # https://search.nixos.org/packages?channel=unstable&type=packages&show=zoxide
     zip # https://search.nixos.org/packages?channel=unstable&type=packages&show=zip
     # keep-sorted end
 
-    # Terminal utilities
+    # Search And Navigation
+    # keep-sorted start block=no newline_separated=no
+    fd # https://search.nixos.org/packages?channel=unstable&type=packages&show=fd
+    fzf # https://search.nixos.org/packages?channel=unstable&type=packages&show=fzf
+    ripgrep # https://search.nixos.org/packages?channel=unstable&type=packages&show=ripgrep
+    zoxide # https://search.nixos.org/packages?channel=unstable&type=packages&show=zoxide
+    # keep-sorted end
+
+    # Terminal Utilities
     # keep-sorted start block=no newline_separated=no
     bat # https://search.nixos.org/packages?channel=unstable&type=packages&show=bat
     btop # https://search.nixos.org/packages?channel=unstable&type=packages&show=btop
     eza # https://search.nixos.org/packages?channel=unstable&type=packages&show=eza
     htop # https://search.nixos.org/packages?channel=unstable&type=packages&show=htop
+    lsd # https://search.nixos.org/packages?channel=unstable&type=packages&show=lsd
+    starship # https://search.nixos.org/packages?channel=unstable&type=packages&show=starship
     tree # https://search.nixos.org/packages?channel=unstable&type=packages&show=tree
     watch # https://search.nixos.org/packages?channel=unstable&type=packages&show=watch
     # keep-sorted end
 
-    # Git related (managed through packages/git but including core here)
+    # Git Related (Managed Through Packages Git But Including Core Here)
     # keep-sorted start block=no newline_separated=no
     gh # https://search.nixos.org/packages?channel=unstable&type=packages&show=gh
     lazygit # https://search.nixos.org/packages?channel=unstable&type=packages&show=lazygit
     # keep-sorted end
 
-    # Network utilities
+    # Network Utilities
     # keep-sorted start block=no newline_separated=no
     bind.dnsutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=dnsutils
     whois # https://search.nixos.org/packages?channel=unstable&type=packages&show=whois
     # keep-sorted end
 
-    # Nix related utilities
+    # Nix Related Utilities
     # keep-sorted start block=no newline_separated=no
     nh # https://search.nixos.org/packages?channel=unstable&type=packages&show=nh
     nil # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
