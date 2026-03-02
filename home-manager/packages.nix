bffb9fe9a84c745fa20fc6cf97841f3458a7b3c6
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-03-02 12:13:10 -0800

    chore(nix): enable azure-cli
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -203,7 +203,7 @@
 
       # Cloud And Infrastructure
       # keep-sorted start block=no newline_separated=no sticky_comments=no
-      # azure-cli # FIXME: Build failure due to missing azure.mgmt.web.v2024_11_01 module. # https://search.nixos.org/packages?channel=unstable&type=packages&show=azure-cli
+      azure-cli # https://search.nixos.org/packages?channel=unstable&type=packages&show=azure-cli
       lego # https://search.nixos.org/packages?channel=unstable&type=packages&show=lego
       rclone # TODO: See `nixpkgs.overlays` in other file. # https://search.nixos.org/packages?channel=unstable&type=packages&show=rclone
       # keep-sorted end
