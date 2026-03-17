1025be0fd30c342782a8ef5b5265b50a27cc8c57
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-03-17 15:17:37 +0100

    refactor(nix): packages
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/development.nix
+++ home-manager/development.nix
@@ -2,19 +2,17 @@
 
 # https://github.com/hardselius/dotfiles/blob/b801fd8aba017a588ce56430d8345449ec396c96/home/packages.nix
 
-_: { }
+# _: { }
 
-# { pkgs, ... }:
-# {
-#   home.packages =
-#     with pkgs;
-#     [
-#       code-cursor # https://search.nixos.org/packages?channel=unstable&type=packages&show=code-cursor
-#       # claude-code # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=claude-code
-#     ]
-#     ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ ])
-#     ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-#       vscodium # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.claude-code.enable # https://search.nixos.org/packages?channel=unstable&type=packages&show=vscodium
-#     ])
-#     ++ (pkgs.lib.optionals (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) [ ]);
-# }
+{ pkgs, ... }:
+{
+  home.packages =
+    with pkgs;
+    [
+      code-cursor # https://search.nixos.org/packages?channel=unstable&type=packages&show=code-cursor
+      vscodium # https://search.nixos.org/packages?channel=unstable&type=packages&show=vscodium
+    ]
+    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ ])
+    ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [ ])
+    ++ (pkgs.lib.optionals (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) [ ]);
+}
