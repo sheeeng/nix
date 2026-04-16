c2a0552c4ea2f28f4200806f01c38f3fc8c36c21
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-04-16 11:48:53 +0200

    chore(nix): nushell package
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -2,7 +2,8 @@
 {
   programs.nushell = {
     # @upstream-issue https://github.com/NixOS/nixpkgs/issues/485915
-    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.enable
+    # @upstream-issue https://github.com/NixOS/nixpkgs/issues/510488
+    enable = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.enable
     package = pkgs.nushell; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.package
     configFile = null; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.configFile
     envFile = null; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.nushell.envFile
