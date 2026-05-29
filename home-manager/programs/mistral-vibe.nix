7f5faf7efc0657e7626c89758873ca72e9b7bf18
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-05-29 14:08:56 +0200

    chore(mistral-vibe): edit settings
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/mistral-vibe.nix
+++ home-manager/programs/mistral-vibe.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.mistral-vibe = {
-    enable = true; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mistral-vibe.enable
+    enable = false; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mistral-vibe.enable
     package = pkgs.mistral-vibe; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mistral-vibe.package
     settings = {
       active_model = "devstral-latest";
