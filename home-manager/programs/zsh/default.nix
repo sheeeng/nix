3715cf805ca844668e6560aa1510f20e0296afcc
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-06-09 10:37:50 +0200

    fix(zsh): reorder path variable to append last
    Move $PATH to the end of the path string so that user-specific
    directories take precedence over system paths.

--- home-manager/programs/zsh/default.nix
+++ home-manager/programs/zsh/default.nix
@@ -71,7 +71,7 @@
     envExtra = ''
       export LESSHISTFILE="${config.xdg.dataHome}/.lesshst";
       export CARGO_HOME="${config.xdg.cacheHome}/.cargo"
-      export PATH="$HOME/bin:$PATH:/etc/profiles/per-user/${config.home.username}/bin"
+      export PATH="$HOME/bin:/etc/profiles/per-user/${config.home.username}/bin:$PATH"
       export PATH="$PATH:${config.xdg.dataHome}/.local/bin"
     ''; # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.envExtra
     history = {
