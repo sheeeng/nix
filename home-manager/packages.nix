2212d227c249e706dd7997373959d6bbd0a3c211
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-06-30 10:06:38 +0200

    chore(packages): disable podman and podman-compose

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -373,9 +373,9 @@
     ])
     ++ (pkgs.lib.optionals (pkgs.stdenv.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) [
       # keep-sorted start block=no newline_separated=no sticky_comments=no
+      # podman # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman
+      # podman-compose # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman-compose
       opentofu # https://search.nixos.org/packages?channel=unstable&type=packages&show=opentofu
-      podman # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman
-      podman-compose # https://search.nixos.org/packages?channel=unstable&type=packages&show=podman-compose
       # keep-sorted end
     ]);
 }
