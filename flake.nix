e0b18abe77f6a067d2a1c447d5b12039a5e4da85
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-07-17 13:36:39 +0200

    chore(nix): update nixpkgs-darwin to unstable
    Switch `nixpkgs-darwin` from the pinned commit `6bfaf02` to the
    `nixpkgs-unstable` branch. The starship Darwin linker fix (#540463)
    has been merged into `nixpkgs-unstable` as of 2026-07-11, so tracking
    the branch is now safe.
    
    The previously pinned commit caused an `opencode` 1.18.3 node_modules
    hash mismatch, which is avoided by moving to `nixpkgs-unstable`.
    
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -94,8 +94,10 @@
     # nixpkgs-darwin.url = "github:nixos/nixpkgs?branch=staging-next&rev=dace194d4791e7dec990c0671795f1e73ff4d196"; # https://github.com/nixos/nixpkgs/issues/449970
     # FAIL (libimagequant Cargo.lock git object missing at nixos-unstable rev 9ae611a455b90cf061d8f332b977e387bda8e1ca)
     # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixos-unstable";
-    # OK (nixpkgs master; starship Darwin linker fix) # https://github.com/NixOS/nixpkgs/pull/540463
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/6bfaf02a46540dad5b083e46e80d9ac133260cd3"; # https://github.com/NixOS/nixpkgs/pull/540463
+    # OK (starship Darwin linker fix #540463 merged 2026-07-11 into nixpkgs-unstable; opencode 1.18.3 node_modules hash mismatch in 6bfaf02 avoided)
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # FAIL (opencode 1.18.3 node_modules hash mismatch on nixpkgs master 6bfaf02) # @upstream-issue TODO
+    # nixpkgs-darwin.url = "github:nixos/nixpkgs/6bfaf02a46540dad5b083e46e80d9ac133260cd3"; # https://github.com/NixOS/nixpkgs/pull/540463
     # FAIL (starship cctools ld64 linker crash on aarch64-darwin) # @upstream-issue https://github.com/NixOS/nixpkgs/issues/540450 # https://github.com/NixOS/nixpkgs/pull/540463
     # nixpkgs-darwin.url = "github:nixos/nixpkgs/dc5eccaaeada7995d3b817e7d7c3bf3ab30b9b20"; # https://github.com/NixOS/nixpkgs/pull/540311
     # FAIL (opencode 1.17.15 node_modules CA hash mismatch) # https://github.com/NixOS/nixpkgs/pull/540311
