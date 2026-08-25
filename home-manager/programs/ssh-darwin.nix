2d0b62edf8034dbc52f822a3c05b9975bc2cb69c
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-08-25 14:58:40 +0200

    fix(ssh): remove unsupported keychain option
    Remove Apple-only `UseKeychain` settings so non-Apple OpenSSH
    clients can parse the shared SSH configuration.
    
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/ssh-darwin.nix
+++ home-manager/programs/ssh-darwin.nix
@@ -1,11 +1,2 @@
-{ lib, pkgs, ... }:
-{
-  config = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-    programs.ssh.settings = {
-      "*".IgnoreUnknown = [ "UseKeychain" ]; # https://man.openbsd.org/ssh_config#IgnoreUnknown
-      "*".UseKeychain = true; # https://man.openbsd.org/ssh_config#UseKeychain
-      "github.com".IgnoreUnknown = [ "UseKeychain" ]; # https://man.openbsd.org/ssh_config#IgnoreUnknown
-      "github.com".UseKeychain = true; # https://man.openbsd.org/ssh_config#UseKeychain
-    };
-  };
-}
+{ ... }:
+{ }
