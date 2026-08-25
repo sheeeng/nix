2ffcf3f2162d57277c0c795f5400e04793b10618
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-08-25 14:06:31 +0200

    fix(ssh): add ignoreunknown for usekeychain
    Suppress warnings when `UseKeychain` is not recognized by adding it
    to `IgnoreUnknown` for both the wildcard host and `github.com`.
    
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- home-manager/programs/ssh-darwin.nix
+++ home-manager/programs/ssh-darwin.nix
@@ -2,7 +2,9 @@
 {
   config = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
     programs.ssh.settings = {
+      "*".IgnoreUnknown = [ "UseKeychain" ]; # https://man.openbsd.org/ssh_config#IgnoreUnknown
       "*".UseKeychain = true; # https://man.openbsd.org/ssh_config#UseKeychain
+      "github.com".IgnoreUnknown = [ "UseKeychain" ]; # https://man.openbsd.org/ssh_config#IgnoreUnknown
       "github.com".UseKeychain = true; # https://man.openbsd.org/ssh_config#UseKeychain
     };
   };
