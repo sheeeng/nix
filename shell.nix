284ee8ea7567872096585adab74c13a2111700d9
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-08-28 21:44:45 +0200

    fix(shell): add ruamel-yaml to minimal-shell
    The `check-yaml` pre-commit hook requires `ruamel.yaml` to run.
    `standard-shell` already included `python313.withPackages
    [ruamel-yaml]`, but `minimal-shell` did not. The "Run Pre-Commit
    via Nix" workflow uses `minimal-shell`, so the hook crashed with
    `ModuleNotFoundError: No module named ruamel` on every run.
    
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -73,20 +73,28 @@
   minimal-shell = pkgs.mkShell {
     name = "minimal-shell";
 
-    buildInputs = with pkgs; [
-      # keep-sorted start block=no newline_separated=no sticky_comments=no
-      (lib.hiPrio uutils-coreutils-noprefix) # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-coreutils-noprefix
-      cabal-install # https://search.nixos.org/packages?channel=unstable&type=packages&show=cabal-install
-      ghc # https://search.nixos.org/packages?channel=unstable&type=packages&show=ghc
-      git # https://search.nixos.org/packages?channel=unstable&type=packages&show=git
-      gmp # https://search.nixos.org/packages?channel=unstable&type=packages&show=gmp
-      gnupg # https://search.nixos.org/packages?channel=unstable&type=packages&show=gnupg
-      nix # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix
-      nixfmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixfmt
-      pre-commit # https://search.nixos.org/packages?channel=unstable&type=packages&show=pre-commit
-      vim # https://search.nixos.org/packages?channel=unstable&type=packages&show=vim
-      # keep-sorted end
-    ];
+    buildInputs =
+      (with pkgs; [
+        # keep-sorted start block=no newline_separated=no sticky_comments=no
+        (lib.hiPrio uutils-coreutils-noprefix) # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-coreutils-noprefix
+        cabal-install # https://search.nixos.org/packages?channel=unstable&type=packages&show=cabal-install
+        ghc # https://search.nixos.org/packages?channel=unstable&type=packages&show=ghc
+        git # https://search.nixos.org/packages?channel=unstable&type=packages&show=git
+        gmp # https://search.nixos.org/packages?channel=unstable&type=packages&show=gmp
+        gnupg # https://search.nixos.org/packages?channel=unstable&type=packages&show=gnupg
+        nix # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix
+        nixfmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixfmt
+        pre-commit # https://search.nixos.org/packages?channel=unstable&type=packages&show=pre-commit
+        vim # https://search.nixos.org/packages?channel=unstable&type=packages&show=vim
+        # keep-sorted end
+      ])
+      ++ [
+        (pkgs.python313.withPackages (
+          ps: with ps; [
+            ruamel-yaml # https://search.nixos.org/packages?channel=unstable&type=packages&show=python313Packages.ruamel-yaml
+          ]
+        ))
+      ];
 
     shellHook = ''
       # Prioritize uutils-coreutils by prepending to $PATH environment variable.
