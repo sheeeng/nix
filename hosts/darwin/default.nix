e9005e1ba5f8cefe9508f7fa9c554d6132f607c1
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-09-04 21:39:12 +0200

    fix(darwin): gate container on arm64
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -5,26 +5,30 @@
     ../../modules/home-manager.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    # keep-sorted start
-    container # https://search.nixos.org/packages?channel=unstable&type=packages&show=container
-    dix # https://search.nixos.org/packages?channel=unstable&type=packages&show=dix
-    manix # https://search.nixos.org/packages?channel=unstable&type=packages&show=manix
-    nh # https://search.nixos.org/packages?channel=unstable&type=packages&show=nh
-    nil # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
-    # nix # Determinate Nix provides Nix on Darwin. Installing this package would shadow it and cause unsupported setting warnings. https://search.nixos.org/packages?channel=unstable&type=packages&show=nix
-    nix-output-monitor # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-output-monitor
-    nix-prefetch-git # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-git
-    nix-prefetch-github # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-github
-    nix-prefetch-scripts # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-scripts
-    nixd # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixd
-    nixfmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixfmt
-    nvd # https://search.nixos.org/packages?channel=unstable&type=packages&show=nvd
-    uutils-coreutils-noprefix # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-coreutils-noprefix
-    uutils-diffutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-diffutils
-    uutils-findutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-findutils
-    uutils-sed # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-sed
-    uutils-tar # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-tar
-    # keep-sorted end
-  ]; # https://nix-darwin.github.io/nix-darwin/manual/#opt-environment.systemPackages
+  environment.systemPackages =
+    with pkgs;
+    lib.optionals (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") [
+      container # https://search.nixos.org/packages?channel=unstable&type=packages&show=container
+    ]
+    ++ [
+      # keep-sorted start
+      dix # https://search.nixos.org/packages?channel=unstable&type=packages&show=dix
+      manix # https://search.nixos.org/packages?channel=unstable&type=packages&show=manix
+      nh # https://search.nixos.org/packages?channel=unstable&type=packages&show=nh
+      nil # https://search.nixos.org/packages?channel=unstable&type=packages&show=nil
+      # nix # Determinate Nix provides Nix on Darwin. Installing this package would shadow it and cause unsupported setting warnings. https://search.nixos.org/packages?channel=unstable&type=packages&show=nix
+      nix-output-monitor # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-output-monitor
+      nix-prefetch-git # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-git
+      nix-prefetch-github # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-github
+      nix-prefetch-scripts # https://search.nixos.org/packages?channel=unstable&type=packages&show=nix-prefetch-scripts
+      nixd # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixd
+      nixfmt # https://search.nixos.org/packages?channel=unstable&type=packages&show=nixfmt
+      nvd # https://search.nixos.org/packages?channel=unstable&type=packages&show=nvd
+      uutils-coreutils-noprefix # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-coreutils-noprefix
+      uutils-diffutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-diffutils
+      uutils-findutils # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-findutils
+      uutils-sed # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-sed
+      uutils-tar # https://search.nixos.org/packages?channel=unstable&type=packages&show=uutils-tar
+      # keep-sorted end
+    ]; # https://nix-darwin.github.io/nix-darwin/manual/#opt-environment.systemPackages
 }
